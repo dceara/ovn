dceara/ovn

commit cc4d5520064f294d2b011be10ec5ff5f1a85bfd0

Support configuring Load Balancer hairpin source IP.
In case traffic that gets load balanced is DNAT-ed to a backend IP that
happens to be the source of the traffic then OVN performs an additional
SNAT to ensure that return traffic is directed through OVN.

Until now the load balancer VIP was chosen as SNAT IP.  However, in
specific scenarios, the CMS may prefer a different IP, e.g., a single
cluster-wide IP.  This commit adds support, through the newly added
Load_Balancer.option 'hairpin_snat_ip', to allow the CMS to explicitly
chose a SNAT IP.

Due to the fact that now traffic that was hairpinned might need to be
SNAT-ed to different IPs for different load balancers that share the
same VIP address value we need to also explicitly match on L4 protocol
and ports in the 'OFTABLE_CT_SNAT_FOR_VIP' table.

Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

lib/lb.c
@@ -170,6 +170,24 @@ void ovn_northd_lb_vip_destroy(struct ovn_northd_lb_vip *vip)
     free(vip->backends_nb);
 }
 
+static void
+ovn_lb_get_hairpin_snat_ip(const struct uuid *lb_uuid,
+                           const struct smap *lb_options,
+                           struct lport_addresses *hairpin_addrs)
+{
+    const char *addresses = smap_get(lb_options, "hairpin_snat_ip");
+
+    if (!addresses) {
+        return;
+    }
+
+    if (!extract_ip_address(addresses, hairpin_addrs)) {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
+        VLOG_WARN_RL(&rl, "bad hairpin_snat_ip %s in load balancer "UUID_FMT,
+                     addresses, UUID_ARGS(lb_uuid));
+    }
+}
+
 struct ovn_northd_lb *
 ovn_northd_lb_create(const struct nbrec_load_balancer *nbrec_lb,
                      struct hmap *ports,
@@ -224,6 +242,9 @@ ovn_northd_lb_create(const struct nbrec_load_balancer *nbrec_lb,
         ds_chomp(&sel_fields, ',');
         lb->selection_fields = ds_steal_cstr(&sel_fields);
     }
+
+    ovn_lb_get_hairpin_snat_ip(&nbrec_lb->header_.uuid, &nbrec_lb->options,
+                               &lb->hairpin_snat_ips);
     return lb;
 }
 
@@ -260,6 +281,7 @@ ovn_northd_lb_destroy(struct ovn_northd_lb *lb)
     free(lb->vips);
     free(lb->vips_nb);
     free(lb->selection_fields);
+    destroy_lport_addresses(&lb->hairpin_snat_ips);
     free(lb->dps);
     free(lb);
 }
@@ -289,6 +311,9 @@ ovn_controller_lb_create(const struct sbrec_load_balancer *sbrec_lb)
      * correct value.
      */
     lb->n_vips = n_vips;
+
+    ovn_lb_get_hairpin_snat_ip(&sbrec_lb->header_.uuid, &sbrec_lb->options,
+                               &lb->hairpin_snat_ips);
     return lb;
 }
 
@@ -299,5 +324,6 @@ ovn_controller_lb_destroy(struct ovn_controller_lb *lb)
         ovn_lb_vip_destroy(&lb->vips[i]);
     }
     free(lb->vips);
+    destroy_lport_addresses(&lb->hairpin_snat_ips);
     free(lb);
 }

lib/lb.h
@@ -20,6 +20,7 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include "openvswitch/hmap.h"
+#include "ovn-util.h"
 
 struct nbrec_load_balancer;
 struct sbrec_load_balancer;
@@ -36,6 +37,11 @@ struct ovn_northd_lb {
     struct ovn_lb_vip *vips;
     struct ovn_northd_lb_vip *vips_nb;
     size_t n_vips;
+
+    struct lport_addresses hairpin_snat_ips; /* IP (v4 and/or v6) to be used
+                                              * as source for hairpinned
+                                              * traffic.
+                                              */
 
     size_t n_dps;
     size_t n_allocated_dps;
@@ -89,6 +95,11 @@ struct ovn_controller_lb {
 
     struct ovn_lb_vip *vips;
     size_t n_vips;
+
+    struct lport_addresses hairpin_snat_ips; /* IP (v4 and/or v6) to be used
+                                              * as source for hairpinned
+                                              * traffic.
+                                              */
 };
 
 struct ovn_controller_lb *ovn_controller_lb_create(

controller/lflow.c
@@ -1189,26 +1189,30 @@ add_lb_vip_hairpin_flows(struct ovn_controller_lb *lb,
     struct match hairpin_reply_match = MATCH_CATCHALL_INITIALIZER;
 
     if (IN6_IS_ADDR_V4MAPPED(&lb_vip->vip)) {
-        ovs_be32 ip4 = in6_addr_get_mapped_ipv4(&lb_backend->ip);
+        ovs_be32 bip4 = in6_addr_get_mapped_ipv4(&lb_backend->ip);
+        ovs_be32 vip4 = lb->hairpin_snat_ips.n_ipv4_addrs
+                        ? lb->hairpin_snat_ips.ipv4_addrs[0].addr
+                        : in6_addr_get_mapped_ipv4(&lb_vip->vip);
 
         match_set_dl_type(&hairpin_match, htons(ETH_TYPE_IP));
-        match_set_nw_src(&hairpin_match, ip4);
-        match_set_nw_dst(&hairpin_match, ip4);
+        match_set_nw_src(&hairpin_match, bip4);
+        match_set_nw_dst(&hairpin_match, bip4);
 
-        match_set_dl_type(&hairpin_reply_match,
-                          htons(ETH_TYPE_IP));
-        match_set_nw_src(&hairpin_reply_match, ip4);
-        match_set_nw_dst(&hairpin_reply_match,
-                         in6_addr_get_mapped_ipv4(&lb_vip->vip));
+        match_set_dl_type(&hairpin_reply_match, htons(ETH_TYPE_IP));
+        match_set_nw_src(&hairpin_reply_match, bip4);
+        match_set_nw_dst(&hairpin_reply_match, vip4);
     } else {
+        struct in6_addr *bip6 = &lb_backend->ip;
+        struct in6_addr *vip6 = lb->hairpin_snat_ips.n_ipv6_addrs
+                                ? &lb->hairpin_snat_ips.ipv6_addrs[0].addr
+                                : &lb_vip->vip;
         match_set_dl_type(&hairpin_match, htons(ETH_TYPE_IPV6));
-        match_set_ipv6_src(&hairpin_match, &lb_backend->ip);
-        match_set_ipv6_dst(&hairpin_match, &lb_backend->ip);
+        match_set_ipv6_src(&hairpin_match, bip6);
+        match_set_ipv6_dst(&hairpin_match, bip6);
 
-        match_set_dl_type(&hairpin_reply_match,
-                          htons(ETH_TYPE_IPV6));
-        match_set_ipv6_src(&hairpin_reply_match, &lb_backend->ip);
-        match_set_ipv6_dst(&hairpin_reply_match, &lb_vip->vip);
+        match_set_dl_type(&hairpin_reply_match, htons(ETH_TYPE_IPV6));
+        match_set_ipv6_src(&hairpin_reply_match, bip6);
+        match_set_ipv6_dst(&hairpin_reply_match, vip6);
     }
 
     if (lb_backend->port) {
@@ -1254,6 +1258,7 @@ add_lb_vip_hairpin_flows(struct ovn_controller_lb *lb,
 static void
 add_lb_ct_snat_vip_flows(struct ovn_controller_lb *lb,
                          struct ovn_lb_vip *lb_vip,
+                         uint8_t lb_proto,
                          struct ovn_desired_flow_table *flow_table)
 {
     uint64_t stub[1024 / 8];
@@ -1277,10 +1282,16 @@ add_lb_ct_snat_vip_flows(struct ovn_controller_lb *lb,
 
     if (IN6_IS_ADDR_V4MAPPED(&lb_vip->vip)) {
         nat->range_af = AF_INET;
-        nat->range.addr.ipv4.min = in6_addr_get_mapped_ipv4(&lb_vip->vip);
+        nat->range.addr.ipv4.min =
+            lb->hairpin_snat_ips.n_ipv4_addrs
+            ? lb->hairpin_snat_ips.ipv4_addrs[0].addr
+            : in6_addr_get_mapped_ipv4(&lb_vip->vip);
     } else {
         nat->range_af = AF_INET6;
-        nat->range.addr.ipv6.min = lb_vip->vip;
+        nat->range.addr.ipv6.min
+            = lb->hairpin_snat_ips.n_ipv6_addrs
+            ? lb->hairpin_snat_ips.ipv6_addrs[0].addr
+            : lb_vip->vip;
     }
     ofpacts.header = ofpbuf_push_uninit(&ofpacts, nat_offset);
     ofpact_finish(&ofpacts, &ct->ofpact);
@@ -1288,11 +1299,15 @@ add_lb_ct_snat_vip_flows(struct ovn_controller_lb *lb,
     struct match match = MATCH_CATCHALL_INITIALIZER;
     if (IN6_IS_ADDR_V4MAPPED(&lb_vip->vip)) {
         match_set_dl_type(&match, htons(ETH_TYPE_IP));
-        match_set_ct_nw_dst(&match, nat->range.addr.ipv4.min);
+        match_set_ct_nw_dst(&match, in6_addr_get_mapped_ipv4(&lb_vip->vip));
     } else {
         match_set_dl_type(&match, htons(ETH_TYPE_IPV6));
         match_set_ct_ipv6_dst(&match, &lb_vip->vip);
     }
+
+    match_set_nw_proto(&match, lb_proto);
+    match_set_ct_nw_proto(&match, lb_proto);
+    match_set_ct_tp_dst(&match, htons(lb_vip->vip_port));
 
     uint32_t ct_state = OVS_CS_F_TRACKED | OVS_CS_F_DST_NAT;
     match_set_ct_state_masked(&match, ct_state, ct_state);
@@ -1349,7 +1364,7 @@ consider_lb_hairpin_flows(const struct sbrec_load_balancer *sbrec_lb,
                                      flow_table);
         }
 
-        add_lb_ct_snat_vip_flows(lb, lb_vip, flow_table);
+        add_lb_ct_snat_vip_flows(lb, lb_vip, lb_proto, flow_table);
     }
 
     ovn_controller_lb_destroy(lb);

lib/ovn-util.c
@@ -232,6 +232,27 @@ extract_ip_addresses(const char *address, struct lport_addresses *laddrs)
     return false;
 }
 
+/* Extracts at most one IPv4 and at most one IPv6 address from 'address'
+ * which should be of the format 'IP1 [IP2]'.
+ *
+ * Return true if at most one IPv4 address and at most one IPv6 address
+ * is found in 'address'.  IPs must be host IPs, i.e., no unmasked bits.
+ *
+ * The caller must call destroy_lport_addresses().
+ */
+bool extract_ip_address(const char *address, struct lport_addresses *laddrs)
+{
+    if (!extract_ip_addresses(address, laddrs) ||
+            laddrs->n_ipv4_addrs > 1 ||
+            laddrs->n_ipv6_addrs > 1 ||
+            (laddrs->n_ipv4_addrs && laddrs->ipv4_addrs[0].plen != 32) ||
+            (laddrs->n_ipv6_addrs && laddrs->ipv6_addrs[0].plen != 128)) {
+        destroy_lport_addresses(laddrs);
+        return false;
+    }
+    return true;
+}
+
 /* Extracts the mac, IPv4 and IPv6 addresses from the
  * "nbrec_logical_router_port" parameter 'lrp'.  Stores the IPv4 and
  * IPv6 addresses in the 'ipv4_addrs' and 'ipv6_addrs' fields of

lib/ovn-util.h
@@ -72,6 +72,7 @@ bool extract_addresses(const char *address, struct lport_addresses *,
                        int *ofs);
 bool extract_lsp_addresses(const char *address, struct lport_addresses *);
 bool extract_ip_addresses(const char *address, struct lport_addresses *);
+bool extract_ip_address(const char *address, struct lport_addresses *);
 bool extract_lrp_networks(const struct nbrec_logical_router_port *,
                           struct lport_addresses *);
 bool extract_sbrec_binding_first_mac(const struct sbrec_port_binding *binding,

northd/ovn-northd.c
@@ -3367,6 +3367,7 @@ build_ovn_lbs(struct northd_context *ctx, struct hmap *datapaths,
         sbrec_load_balancer_set_name(lb->slb, lb->nlb->name);
         sbrec_load_balancer_set_vips(lb->slb, &lb->nlb->vips);
         sbrec_load_balancer_set_protocol(lb->slb, lb->nlb->protocol);
+        sbrec_load_balancer_set_options(lb->slb, &lb->nlb->options);
         sbrec_load_balancer_set_datapaths(
             lb->slb, (struct sbrec_datapath_binding **)lb->dps,
             lb->n_dps);
@@ -8354,15 +8355,10 @@ get_force_snat_ip(struct ovn_datapath *od, const char *key_type,
         return false;
     }
 
-    if (!extract_ip_addresses(addresses, laddrs) ||
-        laddrs->n_ipv4_addrs > 1 ||
-        laddrs->n_ipv6_addrs > 1 ||
-        (laddrs->n_ipv4_addrs && laddrs->ipv4_addrs[0].plen != 32) ||
-        (laddrs->n_ipv6_addrs && laddrs->ipv6_addrs[0].plen != 128)) {
+    if (!extract_ip_address(addresses, laddrs)) {
         static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
         VLOG_WARN_RL(&rl, "bad ip %s in options of router "UUID_FMT"",
                      addresses, UUID_ARGS(&od->key));
-        destroy_lport_addresses(laddrs);
         return false;
     }
 
@@ -13620,6 +13616,7 @@ main(int argc, char *argv[])
     add_column_noalert(ovnsb_idl_loop.idl, &sbrec_load_balancer_col_name);
     add_column_noalert(ovnsb_idl_loop.idl, &sbrec_load_balancer_col_vips);
     add_column_noalert(ovnsb_idl_loop.idl, &sbrec_load_balancer_col_protocol);
+    add_column_noalert(ovnsb_idl_loop.idl, &sbrec_load_balancer_col_options);
     add_column_noalert(ovnsb_idl_loop.idl,
                        &sbrec_load_balancer_col_external_ids);
 

ovn-nb.xml
@@ -1644,6 +1644,14 @@
         Please note using <code>--reject</code> option will disable empty_lb
         SB controller event for this load balancer.
       </column>
+
+      <column name="options" key="hairpin_snat_ip">
+        IP to be used as source IP for packets that have been hair-pinned
+        after load balancing.  The default behavior when the option is not set
+        is to use the load balancer VIP as source IP.  This option may have
+        exactly one IPv4 and/or one IPv6 address on it, separated by a space
+        character.
+      </column>
     </group>
   </table>
 

ovn-sb.ovsschema
@@ -1,7 +1,7 @@
 {
     "name": "OVN_Southbound",
-    "version": "20.14.0",
-    "cksum": "1412040198 25748",
+    "version": "20.15.0",
+    "cksum": "539683023 25965",
     "tables": {
         "SB_Global": {
             "columns": {
@@ -482,6 +482,11 @@
                     "type": {"key": {"type": "uuid",
                                      "refTable": "Datapath_Binding"},
                              "min": 0, "max": "unlimited"}},
+                "options": {
+                     "type": {"key": "string",
+                              "value": "string",
+                              "min": 0,
+                              "max": "unlimited"}},
                 "external_ids": {
                     "type": {"key": "string", "value": "string",
                              "min": 0, "max": "unlimited"}}},

commit 83a48cacd025991dd331e02e790a360f781814ac

lb: Remove hairpin_snat_ips from northd load balancers.
The parsed values are not used anywhere so we might as well avoid
parsing them.

Fixes: cc4d5520064f ("Support configuring Load Balancer hairpin source IP.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

lib/lb.c
@@ -242,9 +242,6 @@ ovn_northd_lb_create(const struct nbrec_load_balancer *nbrec_lb,
         ds_chomp(&sel_fields, ',');
         lb->selection_fields = ds_steal_cstr(&sel_fields);
     }
-
-    ovn_lb_get_hairpin_snat_ip(&nbrec_lb->header_.uuid, &nbrec_lb->options,
-                               &lb->hairpin_snat_ips);
     return lb;
 }
 
@@ -281,7 +278,6 @@ ovn_northd_lb_destroy(struct ovn_northd_lb *lb)
     free(lb->vips);
     free(lb->vips_nb);
     free(lb->selection_fields);
-    destroy_lport_addresses(&lb->hairpin_snat_ips);
     free(lb->dps);
     free(lb);
 }

lib/lb.h
@@ -38,11 +38,6 @@ struct ovn_northd_lb {
     struct ovn_northd_lb_vip *vips_nb;
     size_t n_vips;
 
-    struct lport_addresses hairpin_snat_ips; /* IP (v4 and/or v6) to be used
-                                              * as source for hairpinned
-                                              * traffic.
-                                              */
-
     size_t n_dps;
     size_t n_allocated_dps;
     const struct sbrec_datapath_binding **dps;