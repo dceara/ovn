dceara/ovn

commit 4597317f16d1a522fd468dc5339cbe3bb851b60e

northd: Introduce incremental processing for northd
Initial implementation adds a single node (northd). This single
node executes the northd processing pipeline but does not do so
incrementally.

In order to develop incremental processing for northd, the code
will be organised with a .c/.h file for each I-P node following
the naming convention en-<node name>.c/.h. These files will
contain definition of the node data, the main node processing
functions and change handlers (if any). The purpose of these nodes
will be coordination of the nodes work and implemention of the
relevant interfaces to plug into the I-P framework. The actual
work that will be executed by the node will be organised into
a companion file or files. Ideally this file will follow the
naming convention of the node: e.g. en-<node name>.c is
associated with <node name>.c.

Initial node topology sees the northd node dependent on all DB
nodes. This will evolve over time.

Co-authored-by: Numan Siddique <numans@ovn.org>
Signed-off-by: Numan Siddique <numans@ovn.org>
Signed-off-by: Mark Gray <mark.d.gray@redhat.com>
Signed-off-by: Han Zhou <hzhou@ovn.org>

northd/inc-proc-northd.c
@@ -1,0 +1,254 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+
+#include <getopt.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "lib/inc-proc-eng.h"
+#include "lib/ovn-nb-idl.h"
+#include "lib/ovn-sb-idl.h"
+#include "openvswitch/poll-loop.h"
+#include "openvswitch/vlog.h"
+#include "inc-proc-northd.h"
+#include "en-northd.h"
+#include "util.h"
+
+VLOG_DEFINE_THIS_MODULE(inc_proc_northd);
+
+#define NB_NODES \
+    NB_NODE(nb_global, "nb_global") \
+    NB_NODE(copp, "copp") \
+    NB_NODE(logical_switch, "logical_switch") \
+    NB_NODE(logical_switch_port, "logical_switch_port") \
+    NB_NODE(forwarding_group, "forwarding_group") \
+    NB_NODE(address_set, "address_set") \
+    NB_NODE(port_group, "port_group") \
+    NB_NODE(load_balancer, "load_balancer") \
+    NB_NODE(load_balancer_health_check, "load_balancer_health_check") \
+    NB_NODE(acl, "acl") \
+    NB_NODE(logical_router, "logical_router") \
+    NB_NODE(qos, "qos") \
+    NB_NODE(meter, "meter") \
+    NB_NODE(meter_band, "meter_band") \
+    NB_NODE(logical_router_port, "logical_router_port") \
+    NB_NODE(logical_router_static_route, "logical_router_static_route") \
+    NB_NODE(logical_router_policy, "logical_router_policy") \
+    NB_NODE(nat, "nat") \
+    NB_NODE(dhcp_options, "dhcp_options") \
+    NB_NODE(connection, "connection") \
+    NB_NODE(dns, "dns") \
+    NB_NODE(ssl, "ssl") \
+    NB_NODE(gateway_chassis, "gateway_chassis") \
+    NB_NODE(ha_chassis_group, "ha_chassis_group") \
+    NB_NODE(ha_chassis, "ha_chassis") \
+    NB_NODE(bfd, "bfd")
+
+    enum nb_engine_node {
+#define NB_NODE(NAME, NAME_STR) NB_##NAME,
+    NB_NODES
+#undef NB_NODE
+    };
+
+/* Define engine node functions for nodes that represent NB tables
+ *
+ * en_nb_<TABLE_NAME>_run()
+ * en_nb_<TABLE_NAME>_init()
+ * en_nb_<TABLE_NAME>_cleanup()
+ */
+#define NB_NODE(NAME, NAME_STR) ENGINE_FUNC_NB(NAME);
+    NB_NODES
+#undef NB_NODE
+
+#define SB_NODES \
+    SB_NODE(sb_global, "sb_global") \
+    SB_NODE(chassis, "chassis") \
+    SB_NODE(chassis_private, "chassis_private") \
+    SB_NODE(encap, "encap") \
+    SB_NODE(address_set, "address_set") \
+    SB_NODE(port_group, "port_group") \
+    SB_NODE(logical_flow, "logical_flow") \
+    SB_NODE(logical_dp_group, "logical_DP_group") \
+    SB_NODE(multicast_group, "multicast_group") \
+    SB_NODE(meter, "meter") \
+    SB_NODE(meter_band, "meter_band") \
+    SB_NODE(datapath_binding, "datapath_binding") \
+    SB_NODE(port_binding, "port_binding") \
+    SB_NODE(mac_binding, "mac_binding") \
+    SB_NODE(dhcp_options, "dhcp_options") \
+    SB_NODE(dhcpv6_options, "dhcpv6_options") \
+    SB_NODE(connection, "connection") \
+    SB_NODE(ssl, "ssl") \
+    SB_NODE(dns, "dns") \
+    SB_NODE(rbac_role, "rbac_role") \
+    SB_NODE(rbac_permission, "rbac_permission") \
+    SB_NODE(gateway_chassis, "gateway_chassis") \
+    SB_NODE(ha_chassis, "ha_chassis") \
+    SB_NODE(ha_chassis_group, "ha_chassis_group") \
+    SB_NODE(controller_event, "controller_event") \
+    SB_NODE(ip_multicast, "ip_multicast") \
+    SB_NODE(igmp_group, "igmp_group") \
+    SB_NODE(service_monitor, "service_monitor") \
+    SB_NODE(load_balancer, "load_balancer") \
+    SB_NODE(bfd, "bfd") \
+    SB_NODE(fdb, "fdb")
+
+enum sb_engine_node {
+#define SB_NODE(NAME, NAME_STR) SB_##NAME,
+    SB_NODES
+#undef SB_NODE
+};
+
+/* Define engine node functions for nodes that represent SB tables
+ *
+ * en_sb_<TABLE_NAME>_run()
+ * en_sb_<TABLE_NAME>_init()
+ * en_sb_<TABLE_NAME>_cleanup()
+ */
+#define SB_NODE(NAME, NAME_STR) ENGINE_FUNC_SB(NAME);
+    SB_NODES
+#undef SB_NODE
+
+/* Define engine nodes for NB and SB tables
+ *
+ * struct engine_node en_nb_<TABLE_NAME>
+ * struct engine_node en_sb_<TABLE_NAME>
+ *
+ * Define nodes as static to avoid sparse errors.
+ */
+#define NB_NODE(NAME, NAME_STR) static ENGINE_NODE_NB(NAME, NAME_STR);
+    NB_NODES
+#undef NB_NODE
+
+#define SB_NODE(NAME, NAME_STR) static ENGINE_NODE_SB(NAME, NAME_STR);
+    SB_NODES
+#undef SB_NODE
+
+/* Define engine nodes for other nodes. They should be defined as static to
+ * avoid sparse errors. */
+static ENGINE_NODE(northd, "northd");
+
+void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
+                          struct ovsdb_idl_loop *sb)
+{
+    /* Define relationships between nodes where first argument is dependent
+     * on the second argument */
+    engine_add_input(&en_northd, &en_nb_nb_global, NULL);
+    engine_add_input(&en_northd, &en_nb_copp, NULL);
+    engine_add_input(&en_northd, &en_nb_logical_switch, NULL);
+    engine_add_input(&en_northd, &en_nb_logical_switch_port, NULL);
+    engine_add_input(&en_northd, &en_nb_forwarding_group, NULL);
+    engine_add_input(&en_northd, &en_nb_address_set, NULL);
+    engine_add_input(&en_northd, &en_nb_port_group, NULL);
+    engine_add_input(&en_northd, &en_nb_load_balancer, NULL);
+    engine_add_input(&en_northd, &en_nb_load_balancer_health_check, NULL);
+    engine_add_input(&en_northd, &en_nb_acl, NULL);
+    engine_add_input(&en_northd, &en_nb_logical_router, NULL);
+    engine_add_input(&en_northd, &en_nb_qos, NULL);
+    engine_add_input(&en_northd, &en_nb_meter, NULL);
+    engine_add_input(&en_northd, &en_nb_meter_band, NULL);
+    engine_add_input(&en_northd, &en_nb_logical_router_port, NULL);
+    engine_add_input(&en_northd, &en_nb_logical_router_static_route, NULL);
+    engine_add_input(&en_northd, &en_nb_logical_router_policy, NULL);
+    engine_add_input(&en_northd, &en_nb_nat, NULL);
+    engine_add_input(&en_northd, &en_nb_dhcp_options, NULL);
+    engine_add_input(&en_northd, &en_nb_connection, NULL);
+    engine_add_input(&en_northd, &en_nb_dns, NULL);
+    engine_add_input(&en_northd, &en_nb_ssl, NULL);
+    engine_add_input(&en_northd, &en_nb_gateway_chassis, NULL);
+    engine_add_input(&en_northd, &en_nb_ha_chassis_group, NULL);
+    engine_add_input(&en_northd, &en_nb_ha_chassis, NULL);
+    engine_add_input(&en_northd, &en_nb_bfd, NULL);
+
+    engine_add_input(&en_northd, &en_sb_sb_global, NULL);
+    engine_add_input(&en_northd, &en_sb_chassis, NULL);
+    engine_add_input(&en_northd, &en_sb_chassis_private, NULL);
+    engine_add_input(&en_northd, &en_sb_encap, NULL);
+    engine_add_input(&en_northd, &en_sb_address_set, NULL);
+    engine_add_input(&en_northd, &en_sb_port_group, NULL);
+    engine_add_input(&en_northd, &en_sb_logical_flow, NULL);
+    engine_add_input(&en_northd, &en_sb_logical_dp_group, NULL);
+    engine_add_input(&en_northd, &en_sb_multicast_group, NULL);
+    engine_add_input(&en_northd, &en_sb_meter, NULL);
+    engine_add_input(&en_northd, &en_sb_meter_band, NULL);
+    engine_add_input(&en_northd, &en_sb_datapath_binding, NULL);
+    engine_add_input(&en_northd, &en_sb_port_binding, NULL);
+    engine_add_input(&en_northd, &en_sb_mac_binding, NULL);
+    engine_add_input(&en_northd, &en_sb_dhcp_options, NULL);
+    engine_add_input(&en_northd, &en_sb_dhcpv6_options, NULL);
+    engine_add_input(&en_northd, &en_sb_connection, NULL);
+    engine_add_input(&en_northd, &en_sb_ssl, NULL);
+    engine_add_input(&en_northd, &en_sb_dns, NULL);
+    engine_add_input(&en_northd, &en_sb_rbac_role, NULL);
+    engine_add_input(&en_northd, &en_sb_rbac_permission, NULL);
+    engine_add_input(&en_northd, &en_sb_gateway_chassis, NULL);
+    engine_add_input(&en_northd, &en_sb_ha_chassis, NULL);
+    engine_add_input(&en_northd, &en_sb_ha_chassis_group, NULL);
+    engine_add_input(&en_northd, &en_sb_controller_event, NULL);
+    engine_add_input(&en_northd, &en_sb_ip_multicast, NULL);
+    engine_add_input(&en_northd, &en_sb_igmp_group, NULL);
+    engine_add_input(&en_northd, &en_sb_service_monitor, NULL);
+    engine_add_input(&en_northd, &en_sb_load_balancer, NULL);
+    engine_add_input(&en_northd, &en_sb_bfd, NULL);
+    engine_add_input(&en_northd, &en_sb_fdb, NULL);
+
+    struct engine_arg engine_arg = {
+        .nb_idl = nb->idl,
+        .sb_idl = sb->idl,
+    };
+
+    engine_init(&en_northd, &engine_arg);
+}
+
+void inc_proc_northd_run(struct northd_context *ctx,
+                         bool recompute) {
+    engine_set_force_recompute(recompute);
+    engine_init_run();
+
+    struct engine_context eng_ctx = {
+        .ovnnb_idl_txn = ctx->ovnnb_txn,
+        .ovnsb_idl_txn = ctx->ovnsb_txn,
+        .client_ctx = ctx,
+    };
+
+    engine_set_context(&eng_ctx);
+
+    if (ctx->ovnnb_txn && ctx->ovnsb_txn) {
+        engine_run(true);
+    }
+
+    if (!engine_has_run()) {
+        if (engine_need_run()) {
+            VLOG_DBG("engine did not run, force recompute next time.");
+            engine_set_force_recompute(true);
+            poll_immediate_wake();
+        } else {
+            VLOG_DBG("engine did not run, and it was not needed");
+        }
+    } else if (engine_aborted()) {
+        VLOG_DBG("engine was aborted, force recompute next time.");
+        engine_set_force_recompute(true);
+        poll_immediate_wake();
+    } else {
+        engine_set_force_recompute(false);
+    }
+}
+
+void inc_proc_northd_cleanup(void)
+{
+    engine_cleanup();
+    engine_set_context(NULL);
+}

lib/inc-proc-eng.h
@@ -63,15 +63,22 @@
 #define ENGINE_MAX_INPUT 256
 #define ENGINE_MAX_OVSDB_INDEX 256
 
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "compiler.h"
+
 struct engine_context {
     struct ovsdb_idl_txn *ovs_idl_txn;
     struct ovsdb_idl_txn *ovnsb_idl_txn;
+    struct ovsdb_idl_txn *ovnnb_idl_txn;
     void *client_ctx;
 };
 
 /* Arguments to be passed to the engine at engine_init(). */
 struct engine_arg {
     struct ovsdb_idl *sb_idl;
+    struct ovsdb_idl *nb_idl;
     struct ovsdb_idl *ovs_idl;
 };
 
@@ -345,6 +352,11 @@ static void en_##DB_NAME##_##TBL_NAME##_cleanup(void *data OVS_UNUSED) \
     ENGINE_FUNC_OVSDB(sb, TBL_NAME)
 
 /* Macro to define member functions of an engine node which represents
+ * a table of OVN NB DB */
+#define ENGINE_FUNC_NB(TBL_NAME) \
+    ENGINE_FUNC_OVSDB(nb, TBL_NAME)
+
+/* Macro to define member functions of an engine node which represents
  * a table of open_vswitch DB */
 #define ENGINE_FUNC_OVS(TBL_NAME) \
     ENGINE_FUNC_OVSDB(ovs, TBL_NAME)
@@ -356,6 +368,10 @@ static void en_##DB_NAME##_##TBL_NAME##_cleanup(void *data OVS_UNUSED) \
 /* Macro to define an engine node which represents a table of OVN SB DB */
 #define ENGINE_NODE_SB(TBL_NAME, TBL_NAME_STR) \
     ENGINE_NODE_OVSDB(sb, "SB", TBL_NAME, TBL_NAME_STR);
+
+/* Macro to define an engine node which represents a table of OVN NB DB */
+#define ENGINE_NODE_NB(TBL_NAME, TBL_NAME_STR) \
+    ENGINE_NODE_OVSDB(nb, "NB", TBL_NAME, TBL_NAME_STR);
 
 /* Macro to define an engine node which represents a table of open_vswitch
  * DB */

northd/automake.mk
@@ -4,6 +4,10 @@ northd_ovn_northd_SOURCES = \
 	northd/northd.c \
 	northd/northd.h \
 	northd/ovn-northd.c \
+	northd/en-northd.c \
+	northd/en-northd.h \
+	northd/inc-proc-northd.c \
+	northd/inc-proc-northd.h \
 	northd/ipam.c \
 	northd/ipam.h
 northd_ovn_northd_LDADD = \

northd/en-northd.c
@@ -1,0 +1,45 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+
+#include <getopt.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "en-northd.h"
+#include "lib/inc-proc-eng.h"
+#include "northd.h"
+#include "openvswitch/vlog.h"
+
+VLOG_DEFINE_THIS_MODULE(en_northd);
+
+void en_northd_run(struct engine_node *node, void *data OVS_UNUSED)
+{
+    const struct engine_context *eng_ctx = engine_get_context();
+    struct northd_context *ctx = eng_ctx->client_ctx;
+    ovn_db_run(ctx);
+
+    engine_set_node_state(node, EN_UPDATED);
+
+}
+void *en_northd_init(struct engine_node *node OVS_UNUSED,
+                     struct engine_arg *arg OVS_UNUSED)
+{
+    return NULL;
+}
+
+void en_northd_cleanup(void *data OVS_UNUSED)
+{
+}

northd/en-northd.h
@@ -1,0 +1,17 @@
+#ifndef EN_NORTHD_H
+#define EN_NORTHD_H 1
+
+#include <config.h>
+
+#include <getopt.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "lib/inc-proc-eng.h"
+
+void en_northd_run(struct engine_node *node OVS_UNUSED, void *data OVS_UNUSED);
+void *en_northd_init(struct engine_node *node OVS_UNUSED,
+                     struct engine_arg *arg);
+void en_northd_cleanup(void *data);
+
+#endif /* EN_NORTHD_H */

northd/inc-proc-northd.h
@@ -1,0 +1,15 @@
+#ifndef INC_PROC_NORTHD_H
+#define INC_PROC_NORTHD_H 1
+
+#include <config.h>
+
+#include "northd.h"
+#include "ovsdb-idl.h"
+
+void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
+                          struct ovsdb_idl_loop *sb);
+void inc_proc_northd_run(struct northd_context *ctx,
+                         bool recompute);
+void inc_proc_northd_cleanup(void);
+
+#endif /* INC_PROC_NORTHD */

northd/northd.c
@@ -14868,11 +14868,7 @@ ovnsb_db_run(struct northd_context *ctx,
 }
 
 void
-ovn_db_run(struct northd_context *ctx,
-           struct ovsdb_idl_index *sbrec_chassis_by_name,
-           struct ovsdb_idl_index *sbrec_chassis_by_hostname,
-           struct ovsdb_idl_loop *ovnsb_idl_loop,
-           const char *ovn_internal_version)
+ovn_db_run(struct northd_context *ctx)
 {
     struct hmap datapaths, ports;
     struct ovs_list lr_list;
@@ -14882,13 +14878,15 @@ ovn_db_run(struct northd_context *ctx,
     use_parallel_build = ctx->use_parallel_build;
 
     int64_t start_time = time_wall_msec();
+
     stopwatch_start(OVNNB_DB_RUN_STOPWATCH_NAME, time_msec());
-    ovnnb_db_run(ctx, sbrec_chassis_by_name, sbrec_chassis_by_hostname,
-                 ovnsb_idl_loop, &datapaths, &ports, &lr_list, start_time,
-                 ovn_internal_version);
+    ovnnb_db_run(ctx, ctx->sbrec_chassis_by_name,
+                 ctx->sbrec_chassis_by_hostname, ctx->ovnsb_idl_loop,
+                 &datapaths, &ports, &lr_list, start_time,
+                 ctx->ovn_internal_version);
     stopwatch_stop(OVNNB_DB_RUN_STOPWATCH_NAME, time_msec());
     stopwatch_start(OVNSB_DB_RUN_STOPWATCH_NAME, time_msec());
-    ovnsb_db_run(ctx, ovnsb_idl_loop, &ports, start_time);
+    ovnsb_db_run(ctx, ctx->ovnsb_idl_loop, &ports, start_time);
     stopwatch_stop(OVNSB_DB_RUN_STOPWATCH_NAME, time_msec());
     destroy_datapaths_and_ports(&datapaths, &ports, &lr_list);
 }

northd/northd.h
@@ -21,21 +21,20 @@ struct northd_context {
     const char *ovnsb_db;
     struct ovsdb_idl *ovnnb_idl;
     struct ovsdb_idl *ovnsb_idl;
+    struct ovsdb_idl_loop *ovnnb_idl_loop;
+    struct ovsdb_idl_loop *ovnsb_idl_loop;
     struct ovsdb_idl_txn *ovnnb_txn;
     struct ovsdb_idl_txn *ovnsb_txn;
     struct ovsdb_idl_index *sbrec_chassis_by_name;
+    struct ovsdb_idl_index *sbrec_chassis_by_hostname;
     struct ovsdb_idl_index *sbrec_ha_chassis_grp_by_name;
     struct ovsdb_idl_index *sbrec_mcast_group_by_name_dp;
     struct ovsdb_idl_index *sbrec_ip_mcast_by_dp;
 
+    const char *ovn_internal_version;
     bool use_parallel_build;
 };
 
-void
-ovn_db_run(struct northd_context *ctx,
-           struct ovsdb_idl_index *sbrec_chassis_by_name,
-           struct ovsdb_idl_index *sbrec_chassis_by_hostname,
-           struct ovsdb_idl_loop *ovnsb_idl_loop,
-           const char *ovn_internal_version);
+void ovn_db_run(struct northd_context *ctx);
 
 #endif /* NORTHD_H */

commit 8e2c7675b61559144107be8988aeae960117493e

northd: I-P add explicit dependency on Load_Balancer_Group.
The commit that added I-P for northd missed to add this explicit
dependency.  This didn't really affect functionality because:
a. ovn-northd monitors the whole NB database
b. changes to the Load_Balancer_Group table mark rows in Logical_Switch
   table that refer to the updated LB groups as "updated".

However, it's probably better to make this dependency explicit having in
mind that real incremental processing might be implemented in the
future.

Fixes: 4597317f16d1 ("northd: Introduce incremental processing for northd")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

northd/inc-proc-northd.c
@@ -42,6 +42,7 @@ VLOG_DEFINE_THIS_MODULE(inc_proc_northd);
     NB_NODE(address_set, "address_set") \
     NB_NODE(port_group, "port_group") \
     NB_NODE(load_balancer, "load_balancer") \
+    NB_NODE(load_balancer_group, "load_balancer_group") \
     NB_NODE(load_balancer_health_check, "load_balancer_health_check") \
     NB_NODE(acl, "acl") \
     NB_NODE(logical_router, "logical_router") \
@@ -159,6 +160,7 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
     engine_add_input(&en_northd, &en_nb_address_set, NULL);
     engine_add_input(&en_northd, &en_nb_port_group, NULL);
     engine_add_input(&en_northd, &en_nb_load_balancer, NULL);
+    engine_add_input(&en_northd, &en_nb_load_balancer_group, NULL);
     engine_add_input(&en_northd, &en_nb_load_balancer_health_check, NULL);
     engine_add_input(&en_northd, &en_nb_acl, NULL);
     engine_add_input(&en_northd, &en_nb_logical_router, NULL);