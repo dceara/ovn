dceara/ovn

commit 1fa6612ffebfa6d7debb01cc6aaf412df4864968

northd: Add lflow node
Add an additional node that initially does nothing. This serves as a
template for how to add a new node. This node is inserted after
the northd_node.

This node will be updated in a later commit to generate logical
flows for the SBDB.

Signed-off-by: Mark Gray <mark.d.gray@redhat.com>
Signed-off-by: Han Zhou <hzhou@ovn.org>

northd/en-lflow.c
@@ -1,0 +1,76 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+
+#include <getopt.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "en-lflow.h"
+#include "en-northd.h"
+
+#include "lib/inc-proc-eng.h"
+#include "northd.h"
+#include "stopwatch.h"
+#include "lib/stopwatch-names.h"
+#include "timeval.h"
+#include "openvswitch/vlog.h"
+
+VLOG_DEFINE_THIS_MODULE(en_lflow);
+
+void en_lflow_run(struct engine_node *node, void *data OVS_UNUSED)
+{
+    const struct engine_context *eng_ctx = engine_get_context();
+
+    struct lflow_input lflow_input;
+
+    struct northd_data *northd_data = engine_get_input_data("northd", node);
+
+    lflow_input.sbrec_logical_flow_table =
+        EN_OVSDB_GET(engine_get_input("SB_logical_flow", node));
+    lflow_input.sbrec_multicast_group_table =
+        EN_OVSDB_GET(engine_get_input("SB_multicast_group", node));
+    lflow_input.sbrec_igmp_group_table =
+        EN_OVSDB_GET(engine_get_input("SB_igmp_group", node));
+
+    lflow_input.sbrec_mcast_group_by_name_dp =
+           engine_ovsdb_node_get_index(
+                          engine_get_input("SB_multicast_group", node),
+                         "sbrec_mcast_group_by_name");
+
+    lflow_input.datapaths = &northd_data->datapaths;
+    lflow_input.ports = &northd_data->ports;
+    lflow_input.port_groups = &northd_data->port_groups;
+    lflow_input.meter_groups = &northd_data->meter_groups;
+    lflow_input.lbs = &northd_data->lbs;
+    lflow_input.bfd_connections = &northd_data->bfd_connections;
+    lflow_input.ovn_internal_version_changed =
+                      northd_data->ovn_internal_version_changed;
+
+    stopwatch_start(BUILD_LFLOWS_STOPWATCH_NAME, time_msec());
+    build_lflows(&lflow_input, eng_ctx->ovnsb_idl_txn);
+    stopwatch_stop(BUILD_LFLOWS_STOPWATCH_NAME, time_msec());
+
+    engine_set_node_state(node, EN_UPDATED);
+}
+void *en_lflow_init(struct engine_node *node OVS_UNUSED,
+                     struct engine_arg *arg OVS_UNUSED)
+{
+    return NULL;
+}
+
+void en_lflow_cleanup(void *data OVS_UNUSED)
+{
+}

northd/en-northd.c
@@ -51,10 +51,6 @@ void en_northd_run(struct engine_node *node, void *data)
         engine_ovsdb_node_get_index(
             engine_get_input("SB_ha_chassis_group", node),
             "sbrec_ha_chassis_grp_by_name");
-    input_data.sbrec_mcast_group_by_name_dp =
-        engine_ovsdb_node_get_index(
-            engine_get_input("SB_multicast_group", node),
-            "sbrec_mcast_group_by_name");
     input_data.sbrec_ip_mcast_by_dp =
         engine_ovsdb_node_get_index(
             engine_get_input("SB_ip_multicast", node),
@@ -99,10 +95,6 @@ void en_northd_run(struct engine_node *node, void *data)
         EN_OVSDB_GET(engine_get_input("SB_service_monitor", node));
     input_data.sbrec_bfd_table =
         EN_OVSDB_GET(engine_get_input("SB_bfd", node));
-    input_data.sbrec_logical_flow_table =
-        EN_OVSDB_GET(engine_get_input("SB_logical_flow", node));
-    input_data.sbrec_multicast_group_table =
-        EN_OVSDB_GET(engine_get_input("SB_multicast_group", node));
     input_data.sbrec_address_set_table =
         EN_OVSDB_GET(engine_get_input("SB_address_set", node));
     input_data.sbrec_port_group_table =
@@ -113,8 +105,6 @@ void en_northd_run(struct engine_node *node, void *data)
         EN_OVSDB_GET(engine_get_input("SB_dns", node));
     input_data.sbrec_ip_multicast_table =
         EN_OVSDB_GET(engine_get_input("SB_ip_multicast", node));
-    input_data.sbrec_igmp_group_table =
-        EN_OVSDB_GET(engine_get_input("SB_igmp_group", node));
     input_data.sbrec_chassis_private_table =
         EN_OVSDB_GET(engine_get_input("SB_chassis_private", node));
 

northd/inc-proc-northd.c
@@ -28,6 +28,7 @@
 #include "openvswitch/vlog.h"
 #include "inc-proc-northd.h"
 #include "en-northd.h"
+#include "en-lflow.h"
 #include "util.h"
 
 VLOG_DEFINE_THIS_MODULE(inc_proc_northd);
@@ -143,6 +144,7 @@ enum sb_engine_node {
 /* Define engine nodes for other nodes. They should be defined as static to
  * avoid sparse errors. */
 static ENGINE_NODE(northd, "northd");
+static ENGINE_NODE(lflow, "lflow");
 
 void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
                           struct ovsdb_idl_loop *sb)
@@ -182,9 +184,7 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
     engine_add_input(&en_northd, &en_sb_encap, NULL);
     engine_add_input(&en_northd, &en_sb_address_set, NULL);
     engine_add_input(&en_northd, &en_sb_port_group, NULL);
-    engine_add_input(&en_northd, &en_sb_logical_flow, NULL);
     engine_add_input(&en_northd, &en_sb_logical_dp_group, NULL);
-    engine_add_input(&en_northd, &en_sb_multicast_group, NULL);
     engine_add_input(&en_northd, &en_sb_meter, NULL);
     engine_add_input(&en_northd, &en_sb_meter_band, NULL);
     engine_add_input(&en_northd, &en_sb_datapath_binding, NULL);
@@ -202,11 +202,14 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
     engine_add_input(&en_northd, &en_sb_ha_chassis_group, NULL);
     engine_add_input(&en_northd, &en_sb_controller_event, NULL);
     engine_add_input(&en_northd, &en_sb_ip_multicast, NULL);
-    engine_add_input(&en_northd, &en_sb_igmp_group, NULL);
     engine_add_input(&en_northd, &en_sb_service_monitor, NULL);
     engine_add_input(&en_northd, &en_sb_load_balancer, NULL);
     engine_add_input(&en_northd, &en_sb_bfd, NULL);
     engine_add_input(&en_northd, &en_sb_fdb, NULL);
+    engine_add_input(&en_lflow, &en_sb_logical_flow, NULL);
+    engine_add_input(&en_lflow, &en_sb_multicast_group, NULL);
+    engine_add_input(&en_lflow, &en_sb_igmp_group, NULL);
+    engine_add_input(&en_lflow, &en_northd, NULL);
 
     struct engine_arg engine_arg = {
         .nb_idl = nb->idl,
@@ -224,7 +227,7 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
     struct ovsdb_idl_index *sbrec_chassis_by_hostname =
         chassis_hostname_index_create(sb->idl);
 
-    engine_init(&en_northd, &engine_arg);
+    engine_init(&en_lflow, &engine_arg);
 
     engine_ovsdb_node_add_index(&en_sb_chassis,
                                 "sbrec_chassis_by_name",

northd/northd.h
@@ -41,21 +41,17 @@ struct northd_input {
     const struct sbrec_load_balancer_table *sbrec_load_balancer_table;
     const struct sbrec_service_monitor_table *sbrec_service_monitor_table;
     const struct sbrec_bfd_table *sbrec_bfd_table;
-    const struct sbrec_logical_flow_table *sbrec_logical_flow_table;
-    const struct sbrec_multicast_group_table *sbrec_multicast_group_table;
     const struct sbrec_address_set_table *sbrec_address_set_table;
     const struct sbrec_port_group_table *sbrec_port_group_table;
     const struct sbrec_meter_table *sbrec_meter_table;
     const struct sbrec_dns_table *sbrec_dns_table;
     const struct sbrec_ip_multicast_table *sbrec_ip_multicast_table;
-    const struct sbrec_igmp_group_table *sbrec_igmp_group_table;
     const struct sbrec_chassis_private_table *sbrec_chassis_private_table;
 
     /* Indexes */
     struct ovsdb_idl_index *sbrec_chassis_by_name;
     struct ovsdb_idl_index *sbrec_chassis_by_hostname;
     struct ovsdb_idl_index *sbrec_ha_chassis_grp_by_name;
-    struct ovsdb_idl_index *sbrec_mcast_group_by_name_dp;
     struct ovsdb_idl_index *sbrec_ip_mcast_by_dp;
 };
 
@@ -64,12 +60,28 @@ struct northd_data {
     struct hmap datapaths;
     struct hmap ports;
     struct hmap port_groups;
-    struct hmap mcast_groups;
-    struct hmap igmp_groups;
     struct shash meter_groups;
     struct hmap lbs;
     struct hmap bfd_connections;
     struct ovs_list lr_list;
+    bool ovn_internal_version_changed;
+};
+
+struct lflow_input {
+    /* Southbound table references */
+    const struct sbrec_logical_flow_table *sbrec_logical_flow_table;
+    const struct sbrec_multicast_group_table *sbrec_multicast_group_table;
+    const struct sbrec_igmp_group_table *sbrec_igmp_group_table;
+
+    /* Indexes */
+    struct ovsdb_idl_index *sbrec_mcast_group_by_name_dp;
+
+    const struct hmap *datapaths;
+    const struct hmap *ports;
+    const struct hmap *port_groups;
+    const struct shash *meter_groups;
+    const struct hmap *lbs;
+    const struct hmap *bfd_connections;
     bool ovn_internal_version_changed;
 };
 
@@ -82,8 +94,7 @@ void northd_destroy(struct northd_data *data);
 void northd_init(struct northd_data *data);
 void northd_indices_create(struct northd_data *data,
                            struct ovsdb_idl *ovnsb_idl);
-void build_lflows(struct northd_input *input_data,
-                  struct northd_data *data,
+void build_lflows(struct lflow_input *input_data,
                   struct ovsdb_idl_txn *ovnsb_txn);
 
 #endif /* NORTHD_H */

northd/automake.mk
@@ -6,6 +6,8 @@ northd_ovn_northd_SOURCES = \
 	northd/ovn-northd.c \
 	northd/en-northd.c \
 	northd/en-northd.h \
+	northd/en-lflow.c \
+	northd/en-lflow.h \
 	northd/inc-proc-northd.c \
 	northd/inc-proc-northd.h \
 	northd/ipam.c \

northd/en-lflow.h
@@ -1,0 +1,16 @@
+#ifndef EN_LFLOW_H
+#define EN_LFLOW_H 1
+
+#include <config.h>
+
+#include <getopt.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "lib/inc-proc-eng.h"
+
+void en_lflow_run(struct engine_node *node, void *data);
+void *en_lflow_init(struct engine_node *node, struct engine_arg *arg);
+void en_lflow_cleanup(void *data);
+
+#endif /* EN_LFLOW_H */

northd/inc-proc-northd.h
@@ -6,8 +6,6 @@
 #include "northd.h"
 #include "ovsdb-idl.h"
 
-
-
 void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
                           struct ovsdb_idl_loop *sb);
 void inc_proc_northd_run(struct ovsdb_idl_txn *ovnnb_txn,

commit 676c1760a511b87324402f6a1992e8e87472b9d1

northd: I-P: fix BFD dependency.
BFD entries are updated and their ->ref field is set to 'true' when
static routes are parsed, within build_lflows(), in the 'en_lflow' I-P
node.  Therefore we cannot clean up BFD entries in 'en_northd' which is
an input of 'en_lflow'.

To fix this we now move all BFD handling in the 'en_lflow' node.

This fixes the CI failures that we've recently started hitting when
running the ovn-kubernetes jobs, for example:
https://github.com/ovn-org/ovn/runs/4294661249?check_suite_focus=true#step:10:9154

Fixes: 1fa6612ffebf ("northd: Add lflow node")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

northd/en-lflow.c
@@ -38,6 +38,10 @@ void en_lflow_run(struct engine_node *node, void *data OVS_UNUSED)
 
     struct northd_data *northd_data = engine_get_input_data("northd", node);
 
+    lflow_input.nbrec_bfd_table =
+        EN_OVSDB_GET(engine_get_input("NB_bfd", node));
+    lflow_input.sbrec_bfd_table =
+        EN_OVSDB_GET(engine_get_input("SB_bfd", node));
     lflow_input.sbrec_logical_flow_table =
         EN_OVSDB_GET(engine_get_input("SB_logical_flow", node));
     lflow_input.sbrec_multicast_group_table =
@@ -60,7 +64,11 @@ void en_lflow_run(struct engine_node *node, void *data OVS_UNUSED)
                       northd_data->ovn_internal_version_changed;
 
     stopwatch_start(BUILD_LFLOWS_STOPWATCH_NAME, time_msec());
+    build_bfd_table(&lflow_input, eng_ctx->ovnsb_idl_txn,
+                    &northd_data->bfd_connections,
+                    &northd_data->ports);
     build_lflows(&lflow_input, eng_ctx->ovnsb_idl_txn);
+    bfd_cleanup_connections(&lflow_input, &northd_data->bfd_connections);
     stopwatch_stop(BUILD_LFLOWS_STOPWATCH_NAME, time_msec());
 
     engine_set_node_state(node, EN_UPDATED);

northd/en-northd.c
@@ -66,8 +66,6 @@ void en_northd_run(struct engine_node *node, void *data)
         EN_OVSDB_GET(engine_get_input("NB_load_balancer", node));
     input_data.nbrec_port_group_table =
         EN_OVSDB_GET(engine_get_input("NB_port_group", node));
-    input_data.nbrec_bfd_table =
-        EN_OVSDB_GET(engine_get_input("NB_bfd", node));
     input_data.nbrec_address_set_table =
         EN_OVSDB_GET(engine_get_input("NB_address_set", node));
     input_data.nbrec_meter_table =
@@ -93,8 +91,6 @@ void en_northd_run(struct engine_node *node, void *data)
         EN_OVSDB_GET(engine_get_input("SB_load_balancer", node));
     input_data.sbrec_service_monitor_table =
         EN_OVSDB_GET(engine_get_input("SB_service_monitor", node));
-    input_data.sbrec_bfd_table =
-        EN_OVSDB_GET(engine_get_input("SB_bfd", node));
     input_data.sbrec_address_set_table =
         EN_OVSDB_GET(engine_get_input("SB_address_set", node));
     input_data.sbrec_port_group_table =

northd/inc-proc-northd.c
@@ -178,7 +178,6 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
     engine_add_input(&en_northd, &en_nb_gateway_chassis, NULL);
     engine_add_input(&en_northd, &en_nb_ha_chassis_group, NULL);
     engine_add_input(&en_northd, &en_nb_ha_chassis, NULL);
-    engine_add_input(&en_northd, &en_nb_bfd, NULL);
 
     engine_add_input(&en_northd, &en_sb_sb_global, NULL);
     engine_add_input(&en_northd, &en_sb_chassis, NULL);
@@ -206,8 +205,9 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
     engine_add_input(&en_northd, &en_sb_ip_multicast, NULL);
     engine_add_input(&en_northd, &en_sb_service_monitor, NULL);
     engine_add_input(&en_northd, &en_sb_load_balancer, NULL);
-    engine_add_input(&en_northd, &en_sb_bfd, NULL);
     engine_add_input(&en_northd, &en_sb_fdb, NULL);
+    engine_add_input(&en_lflow, &en_nb_bfd, NULL);
+    engine_add_input(&en_lflow, &en_sb_bfd, NULL);
     engine_add_input(&en_lflow, &en_sb_logical_flow, NULL);
     engine_add_input(&en_lflow, &en_sb_multicast_group, NULL);
     engine_add_input(&en_lflow, &en_sb_igmp_group, NULL);

northd/northd.c
@@ -8348,8 +8348,8 @@ bfd_port_lookup(const struct hmap *bfd_map, const char *logical_port,
     return NULL;
 }
 
-static void
-bfd_cleanup_connections(struct northd_input *input_data,
+void
+bfd_cleanup_connections(struct lflow_input *input_data,
                         struct hmap *bfd_map)
 {
     const struct nbrec_bfd *nb_bt;
@@ -8432,8 +8432,8 @@ static int bfd_get_unused_port(unsigned long *bfd_src_ports)
     return port + BFD_UDP_SRC_PORT_START;
 }
 
-static void
-build_bfd_table(struct northd_input *input_data,
+void
+build_bfd_table(struct lflow_input *input_data,
                 struct ovsdb_idl_txn *ovnsb_txn,
                 struct hmap *bfd_connections, struct hmap *ports)
 {
@@ -14935,8 +14935,6 @@ ovnnb_db_run(struct northd_input *input_data,
     build_lrouter_groups(&data->ports, &data->lr_list);
     build_ip_mcast(input_data, ovnsb_txn, &data->datapaths);
     build_meter_groups(input_data, &data->meter_groups);
-    build_bfd_table(input_data,
-                    ovnsb_txn, &data->bfd_connections, &data->ports);
     stopwatch_stop(BUILD_LFLOWS_CTX_STOPWATCH_NAME, time_msec());
     stopwatch_start(CLEAR_LFLOWS_CTX_STOPWATCH_NAME, time_msec());
     ovn_update_ipv6_prefix(&data->ports);
@@ -14946,7 +14944,6 @@ ovnnb_db_run(struct northd_input *input_data,
     sync_meters(input_data, ovnsb_txn, &data->meter_groups);
     sync_dns_entries(input_data, ovnsb_txn, &data->datapaths);
     cleanup_stale_fdp_entries(input_data, &data->datapaths);
-    bfd_cleanup_connections(input_data, &data->bfd_connections);
     stopwatch_stop(CLEAR_LFLOWS_CTX_STOPWATCH_NAME, time_msec());
 }
 

northd/northd.h
@@ -25,7 +25,6 @@ struct northd_input {
     const struct nbrec_logical_router_table *nbrec_logical_router;
     const struct nbrec_load_balancer_table *nbrec_load_balancer_table;
     const struct nbrec_port_group_table *nbrec_port_group_table;
-    const struct nbrec_bfd_table *nbrec_bfd_table;
     const struct nbrec_address_set_table *nbrec_address_set_table;
     const struct nbrec_meter_table *nbrec_meter_table;
     const struct nbrec_acl_table *nbrec_acl_table;
@@ -40,7 +39,6 @@ struct northd_input {
     const struct sbrec_fdb_table *sbrec_fdb_table;
     const struct sbrec_load_balancer_table *sbrec_load_balancer_table;
     const struct sbrec_service_monitor_table *sbrec_service_monitor_table;
-    const struct sbrec_bfd_table *sbrec_bfd_table;
     const struct sbrec_address_set_table *sbrec_address_set_table;
     const struct sbrec_port_group_table *sbrec_port_group_table;
     const struct sbrec_meter_table *sbrec_meter_table;
@@ -68,7 +66,11 @@ struct northd_data {
 };
 
 struct lflow_input {
+    /* Northbound table references */
+    const struct nbrec_bfd_table *nbrec_bfd_table;
+
     /* Southbound table references */
+    const struct sbrec_bfd_table *sbrec_bfd_table;
     const struct sbrec_logical_flow_table *sbrec_logical_flow_table;
     const struct sbrec_multicast_group_table *sbrec_multicast_group_table;
     const struct sbrec_igmp_group_table *sbrec_igmp_group_table;
@@ -95,5 +97,10 @@ void northd_indices_create(struct northd_data *data,
                            struct ovsdb_idl *ovnsb_idl);
 void build_lflows(struct lflow_input *input_data,
                   struct ovsdb_idl_txn *ovnsb_txn);
+void build_bfd_table(struct lflow_input *input_data,
+                     struct ovsdb_idl_txn *ovnsb_txn,
+                     struct hmap *bfd_connections, struct hmap *ports);
+void bfd_cleanup_connections(struct lflow_input *input_data,
+                             struct hmap *bfd_map);
 
 #endif /* NORTHD_H */

tests/ovn-northd.at
@@ -3219,6 +3219,14 @@ wait_column admin_down bfd status logical_port=r0-sw1
 ovn-nbctl destroy bfd $uuid
 wait_row_count bfd 5
 
+# Simulate BFD up in Southbound for an automatically created entry.
+# This entry is referenced so the state in the Northbound should also
+# become "up".
+wait_column down nb:bfd status logical_port=r0-sw2
+bfd2_uuid=$(fetch_column bfd _uuid logical_port=r0-sw2)
+check ovn-sbctl set bfd $bfd2_uuid status=up
+wait_column up nb:bfd status logical_port=r0-sw2
+
 AT_CLEANUP
 ])
 