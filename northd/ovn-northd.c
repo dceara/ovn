dceara/ovn

commit 953832eb17f31374ba3cbc5db99d63b134fc626c

ovn-northd: Don't trigger recompute for transactions that are in progress.
Most of the times ovsdb_idl_loop_commit_and_wait() will return -1
indicating that the newly initiated transaction is still in progress.

That's not an error condition and it shouldn't trigger a forced
recompute.  Moreover, arguably, we shouldn't clear tracked rows only
when the transaction is successful.  We should probably do it
unconditionally in every iteration, to avoid processing the same update
twice.

Fixes: 4597317f16d1 ("northd: Introduce incremental processing for northd")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

northd/ovn-northd.c
@@ -1112,17 +1112,19 @@ main(int argc, char *argv[])
         }
 
         /* If there are any errors, we force a full recompute in order to
-           ensure we handle any new tracked changes. */
-        if (ovsdb_idl_loop_commit_and_wait(&ovnnb_idl_loop) != 1) {
+           ensure we handle all changes. */
+        if (!ovsdb_idl_loop_commit_and_wait(&ovnnb_idl_loop)) {
+            VLOG_INFO("OVNNB commit failed, force recompute next time.");
             recompute = true;
-        } else  {
-            ovsdb_idl_track_clear(ovnnb_idl_loop.idl);
         }
-        if (ovsdb_idl_loop_commit_and_wait(&ovnsb_idl_loop) != 1) {
+
+        if (!ovsdb_idl_loop_commit_and_wait(&ovnsb_idl_loop)) {
+            VLOG_INFO("OVNSB commit failed, force recompute next time.");
             recompute = true;
-        } else {
-            ovsdb_idl_track_clear(ovnsb_idl_loop.idl);
         }
+
+        ovsdb_idl_track_clear(ovnnb_idl_loop.idl);
+        ovsdb_idl_track_clear(ovnsb_idl_loop.idl);
 
         unixctl_server_run(unixctl);
         unixctl_server_wait(unixctl);

commit f67bbdd3bcf36268971e4ae98dc687113b25a0ff

ovn-northd: Don't log transaction failures on standby instances.
We still need to try to ovsdb_idl_loop_commit_and_wait() on instances
that are in standby mode.  That's because they need to try to take the
lock.  But if they're in standby they won't actually build a transaction
json and will return early because they don't own the SB lock.

That's reported as an error by ovsdb_idl_loop_commit_and_wait() but we
shouldn't act on it.

Also, to ensure that no DB changes are missed, ovsdb_idl_track_clear()
should be called only on active instances.  The standby or paused ones
will get the incremental updates when they become active.  Otherwise we
would be forced to perform a full recompute when the instance becomes
active.

Fixes: 953832eb17f3 ("ovn-northd: Don't trigger recompute for transactions that are in progress.")
Fixes: 4597317f16d1 ("northd: Introduce incremental processing for northd")
Reported-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

northd/ovn-northd.c
@@ -1089,6 +1089,26 @@ main(int argc, char *argv[])
                                             ovnnb_txn, ovnsb_txn,
                                             &ovnsb_idl_loop);
                 }
+
+                /* If there are any errors, we force a full recompute in order
+                 * to ensure we handle all changes. */
+                if (!ovsdb_idl_loop_commit_and_wait(&ovnnb_idl_loop)) {
+                    VLOG_INFO("OVNNB commit failed, "
+                              "force recompute next time.");
+                    recompute = true;
+                }
+
+                if (!ovsdb_idl_loop_commit_and_wait(&ovnsb_idl_loop)) {
+                    VLOG_INFO("OVNSB commit failed, "
+                              "force recompute next time.");
+                    recompute = true;
+                }
+                ovsdb_idl_track_clear(ovnnb_idl_loop.idl);
+                ovsdb_idl_track_clear(ovnsb_idl_loop.idl);
+            } else {
+                /* Make sure we send any pending requests, e.g., lock. */
+                ovsdb_idl_loop_commit_and_wait(&ovnnb_idl_loop);
+                ovsdb_idl_loop_commit_and_wait(&ovnsb_idl_loop);
             }
         } else {
             /* ovn-northd is paused
@@ -1111,21 +1131,6 @@ main(int argc, char *argv[])
             ovsdb_idl_wait(ovnnb_idl_loop.idl);
             ovsdb_idl_wait(ovnsb_idl_loop.idl);
         }
-
-        /* If there are any errors, we force a full recompute in order to
-           ensure we handle all changes. */
-        if (!ovsdb_idl_loop_commit_and_wait(&ovnnb_idl_loop)) {
-            VLOG_INFO("OVNNB commit failed, force recompute next time.");
-            recompute = true;
-        }
-
-        if (!ovsdb_idl_loop_commit_and_wait(&ovnsb_idl_loop)) {
-            VLOG_INFO("OVNSB commit failed, force recompute next time.");
-            recompute = true;
-        }
-
-        ovsdb_idl_track_clear(ovnnb_idl_loop.idl);
-        ovsdb_idl_track_clear(ovnsb_idl_loop.idl);
 
         unixctl_server_run(unixctl);
         unixctl_server_wait(unixctl);