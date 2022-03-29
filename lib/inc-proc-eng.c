dceara/ovn

commit e1041d9c0badff789a96b356f1937d31fe925f8e

inc-proc-eng: use VLOG_INFO_RL for recompute time over 500ms
Use VLOG_INFO_RL in engine_recompute and engine_compute routines
to log compute time over a given threshold (i.e. 500ms).
Make timeout threshold configurable through ovs-appctl command:

$ovn-appctl -t ovn-controller inc-engine/compute-log-timeout 100

Reported-at: https://bugzilla.redhat.com/show_bug.cgi?id=1988555
Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Acked-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

lib/inc-proc-eng.c
@@ -47,6 +47,8 @@ static const char *engine_node_state_name[EN_STATE_MAX] = {
     [EN_ABORTED]   = "Aborted",
 };
 
+static long long engine_compute_log_timeout_msec = 500;
+
 static void
 engine_recompute(struct engine_node *node, bool allowed,
                  const char *reason_fmt, ...) OVS_PRINTF_FORMAT(3, 4);
@@ -152,6 +154,20 @@ engine_trigger_recompute_cmd(struct unixctl_conn *conn, int argc OVS_UNUSED,
     unixctl_command_reply(conn, NULL);
 }
 
+static void
+engine_set_log_timeout_cmd(struct unixctl_conn *conn, int argc OVS_UNUSED,
+                           const char *argv[], void *arg OVS_UNUSED)
+{
+
+    unsigned int timeout;
+    if (!str_to_uint(argv[1], 10, &timeout)) {
+        unixctl_command_reply_error(conn, "unsigned integer required");
+        return;
+    }
+    engine_compute_log_timeout_msec = timeout;
+    unixctl_command_reply(conn, NULL);
+}
+
 void
 engine_init(struct engine_node *node, struct engine_arg *arg)
 {
@@ -172,6 +188,8 @@ engine_init(struct engine_node *node, struct engine_arg *arg)
                              engine_clear_stats, NULL);
     unixctl_command_register("inc-engine/recompute", "", 0, 0,
                              engine_trigger_recompute_cmd, NULL);
+    unixctl_command_register("inc-engine/compute-log-timeout", "", 1, 1,
+                             engine_set_log_timeout_cmd, NULL);
 }
 
 void
@@ -361,8 +379,15 @@ engine_recompute(struct engine_node *node, bool allowed,
     long long int now = time_msec();
     node->run(node, node->data);
     node->stats.recompute++;
-    VLOG_DBG("node: %s, recompute (%s) took %lldms", node->name, reason,
-             time_msec() - now);
+    long long int delta_time = time_msec() - now;
+    if (delta_time > engine_compute_log_timeout_msec) {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(20, 10);
+        VLOG_INFO_RL(&rl, "node: %s, recompute (%s) took %lldms", node->name,
+                     reason, delta_time);
+    } else {
+        VLOG_DBG("node: %s, recompute (%s) took %lldms", node->name, reason,
+                 delta_time);
+    }
 done:
     free(reason);
 }
@@ -379,10 +404,17 @@ engine_compute(struct engine_node *node, bool recompute_allowed)
              */
             long long int now = time_msec();
             bool handled = node->inputs[i].change_handler(node, node->data);
-
-            VLOG_DBG("node: %s, handler for input %s took %lldms",
-                     node->name, node->inputs[i].node->name,
-                     time_msec() - now);
+            long long int delta_time = time_msec() - now;
+            if (delta_time > engine_compute_log_timeout_msec) {
+                static struct vlog_rate_limit rl =
+                    VLOG_RATE_LIMIT_INIT(20, 10);
+                VLOG_INFO_RL(&rl, "node: %s, handler for input %s took %lldms",
+                             node->name, node->inputs[i].node->name,
+                             delta_time);
+            } else {
+                VLOG_DBG("node: %s, handler for input %s took %lldms",
+                         node->name, node->inputs[i].node->name, delta_time);
+            }
             if (!handled) {
                 engine_recompute(node, recompute_allowed,
                                  "failed handler for input %s",

commit 5f84e1deab686f63e28f4d299aea6aea4013d9a2

inc-proc-eng: Properly log recompute reason.
The 'reason' was computed only when logging at level DBG.  However, if a
node takes more than 'engine_compute_log_timeout_msec' to recompute, the
log is generated at level INFO, resulting in:

  inc_proc_eng|INFO|node: northd, recompute ((null))

We now unconditionally compute 'reason'.  In theory it's a bit
inefficient but given that the number of I-P nodes is bound and usually
relatively low, there's no real performance hit while the code remains
readable.

Fixes: e1041d9c0bad ("inc-proc-eng: use VLOG_INFO_RL for recompute time over 500ms")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

lib/inc-proc-eng.c
@@ -354,14 +354,11 @@ engine_recompute(struct engine_node *node, bool allowed,
                  const char *reason_fmt, ...)
 {
     char *reason = NULL;
+    va_list reason_args;
 
-    if (VLOG_IS_DBG_ENABLED()) {
-        va_list reason_args;
-
-        va_start(reason_args, reason_fmt);
-        reason = xvasprintf(reason_fmt, reason_args);
-        va_end(reason_args);
-    }
+    va_start(reason_args, reason_fmt);
+    reason = xvasprintf(reason_fmt, reason_args);
+    va_end(reason_args);
 
     if (!allowed) {
         VLOG_DBG("node: %s, recompute (%s) aborted", node->name, reason);