dceara/ovn

commit acec066091c866fe2aa624c512c780d16d51cc6c

northd: Add new NB_Global.options:default_acl_drop option.
This option changes how logical switch ACL related flows are generated
such that the following behavior is ensured:

a. If a logical switch has no ACL applied to it (either directly or
indirectly via a port group) then traffic is always allowed in the
ls_in_acl, ls_in_acl_after_lb, ls_out_acl stages.

b. If a logical switch has ACLs applied (directly or indirectly) and
NB_Global.options:default_acl_drop is set to 'false', then traffic that
doesn't match any ACL in the ls_in_acl, ls_in_acl_after_lb, ls_out_acl
stages is allowed to advance to the next step in the processing
pipeline.

c. If a logical switch has *any* ACL applied (directly or indirectly)
and NB_Global.options:default_acl_drop is set to 'true', then a default
lowest-priority rule is added to the ls_in_acl, ls_in_acl_after_lb,
ls_out_acl stages to drop traffic that is not matched by any ACLs.

The goal of the feature is to simplify the configuration of the ACLs and
port groups for CMSs that require a default-deny firewall
implementation.  One such example is with OpenStack security groups
which, when enabled, implicitly drop all not explicitly allowed traffic.

Until now the CMS had to add all logical ports corresponding to VMs in a
network to a single, huge, default-drop-port-group and apply a single
drop ACL to the port group.

With this new feature, the CMS can enable 'default_acl_drop', and punch
holes for traffic that needs to be allowed.  The resulting NB and SB
configuration is also reduced in size.

Reported-by: Daniel Alvarez Sanchez <dalvarez@redhat.com>
Reported-at: https://bugzilla.redhat.com/show_bug.cgi?id=1947807
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Acked-by: Han Zhou <hzhou@ovn.org>
Signed-off-by: Mark Michelson <mmichels@redhat.com>

northd/northd.c
@@ -74,6 +74,12 @@ static struct eth_addr svc_monitor_mac_ea;
  * Otherwise, it will avoid using it.  The default is true. */
 static bool use_ct_inv_match = true;
 
+/* If this option is 'true' northd will implicitly add a lowest-priority
+ * drop rule in the ACL stage of logical switches that have at least one
+ * ACL.
+ */
+static bool default_acl_drop;
+
 #define MAX_OVN_TAGS 4096
 
 /* Pipeline stages. */
@@ -6630,6 +6636,7 @@ static void
 build_acls(struct ovn_datapath *od, struct hmap *lflows,
            const struct hmap *port_groups, const struct shash *meter_groups)
 {
+    const char *default_acl_action = default_acl_drop ? "drop;" : "next;";
     bool has_stateful = od->has_stateful_acl || od->has_lb_vip;
     struct ds match   = DS_EMPTY_INITIALIZER;
     struct ds actions = DS_EMPTY_INITIALIZER;
@@ -6641,22 +6648,34 @@ build_acls(struct ovn_datapath *od, struct hmap *lflows,
      *
      * A related rule at priority 1 is added below if there
      * are any stateful ACLs in this datapath. */
-    if (!od->has_acls && !od->has_lb_vip) {
-        ovn_lflow_add(lflows, od, S_SWITCH_IN_ACL, UINT16_MAX, "1", "next;");
-        ovn_lflow_add(lflows, od, S_SWITCH_OUT_ACL, UINT16_MAX, "1", "next;");
+    if (!od->has_acls) {
+        if (!od->has_lb_vip) {
+            ovn_lflow_add(lflows, od, S_SWITCH_IN_ACL, UINT16_MAX, "1",
+                          "next;");
+            ovn_lflow_add(lflows, od, S_SWITCH_OUT_ACL, UINT16_MAX, "1",
+                          "next;");
+        } else {
+            ovn_lflow_add(lflows, od, S_SWITCH_IN_ACL, 1, "1", "next;");
+            ovn_lflow_add(lflows, od, S_SWITCH_OUT_ACL, 1, "1", "next;");
+        }
+        ovn_lflow_add(lflows, od, S_SWITCH_IN_ACL_AFTER_LB, 0, "1", "next;");
     } else {
-        ovn_lflow_add(lflows, od, S_SWITCH_IN_ACL, 0, "1", "next;");
-        ovn_lflow_add(lflows, od, S_SWITCH_OUT_ACL, 0, "1", "next;");
+        ovn_lflow_add(lflows, od, S_SWITCH_IN_ACL, 0, "1",
+                      default_acl_action);
+        ovn_lflow_add(lflows, od, S_SWITCH_OUT_ACL, 0, "1",
+                      default_acl_action);
+        ovn_lflow_add(lflows, od, S_SWITCH_IN_ACL_AFTER_LB, 0, "1",
+                      default_acl_action);
     }
 
-    ovn_lflow_add(lflows, od, S_SWITCH_IN_ACL_AFTER_LB, 0, "1", "next;");
 
     if (has_stateful) {
         /* Ingress and Egress ACL Table (Priority 1).
          *
-         * By default, traffic is allowed.  This is partially handled by
-         * the Priority 0 ACL flows added earlier, but we also need to
-         * commit IP flows.  This is because, while the initiater's
+         * By default, traffic is allowed (if default_acl_drop is 'false') or
+         * dropped (if default_acl_drop is 'true').  This is partially
+         * handled by the Priority 0 ACL flows added earlier, but we also
+         * need to commit IP flows.  This is because, while the initiater's
          * direction may not have any stateful rules, the server's may
          * and then its return traffic would not have an associated
          * conntrack entry and would return "+invalid".
@@ -6674,11 +6693,19 @@ build_acls(struct ovn_datapath *od, struct hmap *lflows,
          * Subsequent packets will hit the flow at priority 0 that just
          * uses "next;". */
         ovn_lflow_add(lflows, od, S_SWITCH_IN_ACL, 1,
-                      "ip && (!ct.est || (ct.est && ct_mark.blocked == 1))",
+                      "ip && ct.est && ct_mark.blocked == 1",
                        REGBIT_CONNTRACK_COMMIT" = 1; next;");
         ovn_lflow_add(lflows, od, S_SWITCH_OUT_ACL, 1,
-                      "ip && (!ct.est || (ct.est && ct_mark.blocked == 1))",
+                      "ip && ct.est && ct_mark.blocked == 1",
                        REGBIT_CONNTRACK_COMMIT" = 1; next;");
+
+        default_acl_action = default_acl_drop
+                             ? "drop;"
+                             : REGBIT_CONNTRACK_COMMIT" = 1; next;";
+        ovn_lflow_add(lflows, od, S_SWITCH_IN_ACL, 1, "ip && !ct.est",
+                      default_acl_action);
+        ovn_lflow_add(lflows, od, S_SWITCH_OUT_ACL, 1, "ip && !ct.est",
+                      default_acl_action);
 
         /* Ingress and Egress ACL Table (Priority 65532).
          *
@@ -15337,6 +15364,7 @@ ovnnb_db_run(struct northd_input *input_data,
                                         "controller_event", false);
     check_lsp_is_up = !smap_get_bool(&nb->options,
                                      "ignore_lsp_down", true);
+    default_acl_drop = smap_get_bool(&nb->options, "default_acl_drop", false);
 
     build_datapaths(input_data, ovnsb_txn, &data->datapaths, &data->lr_list);
     build_lbs(input_data, &data->datapaths, &data->lbs);

northd/ovn-northd.8.xml
@@ -774,8 +774,12 @@
     <p>
       This table contains a priority-65535 flow to advance to the next table
       if the logical switch has <code>no</code> ACLs configured, otherwise a
-        priority-0 flow to advance to the next table so that ACLs allow
-        packets by default.
+      priority-0 flow to advance to the next table so that ACLs allow
+      packets by default if <ref column="options:default_acl_drop"
+      table="NB_Global" db="OVN_Northbound"/> colum of <ref table="NB_Global"
+      db="OVN_Northbound"/> is <code>false</code> or not set.  Otherwise
+      the flow action is set to <code>drop;</code> to implement a default
+      drop behavior.
     </p>
 
     <p>
@@ -784,6 +788,26 @@
     </p>
 
     <ul>
+      <li>
+        If <ref column="options:default_acl_drop" table="NB_Global"
+        db="OVN_Northbound"/> colum of <ref table="NB_Global"
+        db="OVN_Northbound"/> is <code>false</code> or not set, a priority-1
+        flow that sets the hint to commit IP traffic that is not part of
+        established sessions to the connection tracker (with action
+        <code>reg0[1] = 1; next;</code>).  This is needed for
+        the default allow policy because, while the initiator's direction
+        may not have any stateful rules, the server's may and then
+        its return traffic would not be known and marked as invalid.
+      </li>
+
+      <li>
+        If <ref column="options:default_acl_drop" table="NB_Global"
+        db="OVN_Northbound"/> colum of <ref table="NB_Global"
+        db="OVN_Northbound"/> is <code>true</code>, a priority-1
+        flow that drops IP traffic that is not part of established
+        sessions.
+      </li>
+
       <li>
         A priority-1 flow that sets the hint to commit IP traffic to the
         connection tracker (with action <code>reg0[1] = 1; next;</code>).  This

ovn-nb.xml
@@ -255,6 +255,14 @@
         </p>
       </column>
 
+      <column name="options" key="default_acl_drop">
+        <p>
+          If set to <code>true</code>., <code>ovn-northd</code> will
+          generate a logical flow to drop all traffic in the ACL stages.
+          By default this option is set to <code>false</code>.
+        </p>
+      </column>
+
       <group title="Options for configuring interconnection route advertisement">
         <p>
           These options control how routes are advertised between OVN

commit 7d868f0acf09c824c307b70ff8f14eef952db6fc

northd: Fix default ACL stage logical flow priorities.
When load balancers are configured there was a bug that was causing
the default IN/OUT_ACL flows to be created with a wrong priority.  This
commit fixes it.

Fixes: acec066091c8 ("northd: Add new NB_Global.options:default_acl_drop option.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

northd/northd.c
@@ -6655,8 +6655,8 @@ build_acls(struct ovn_datapath *od, struct hmap *lflows,
             ovn_lflow_add(lflows, od, S_SWITCH_OUT_ACL, UINT16_MAX, "1",
                           "next;");
         } else {
-            ovn_lflow_add(lflows, od, S_SWITCH_IN_ACL, 1, "1", "next;");
-            ovn_lflow_add(lflows, od, S_SWITCH_OUT_ACL, 1, "1", "next;");
+            ovn_lflow_add(lflows, od, S_SWITCH_IN_ACL, 0, "1", "next;");
+            ovn_lflow_add(lflows, od, S_SWITCH_OUT_ACL, 0, "1", "next;");
         }
         ovn_lflow_add(lflows, od, S_SWITCH_IN_ACL_AFTER_LB, 0, "1", "next;");
     } else {