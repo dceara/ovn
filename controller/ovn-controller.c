19dbd881edcbd5c0498448d35eefa53ab4082518
Ihar Hrachyshka <ihrachys@redhat.com>
2022-12-01 02:26:38 +0000

    Support passing chassis name via CLI
    This patch adds support for the desired system-id (chassis name) to be
    passed via CLI:
    
    $ ovn-controller -n <chassis-name>
    
    If passed, CLI overrides any settings stored in ovsdb or in
    system-id-override file.
    
    This may be useful when running multiple controller instances using the
    same vswitchd instance.
    
    Signed-off-by: Ihar Hrachyshka <ihrachys@redhat.com>

--- controller/ovn-controller.c
+++ controller/ovn-controller.c
@@ -4975,6 +4975,9 @@ loop_done:
     if (file_system_id) {
         free(file_system_id);
     }
+    if (cli_system_id) {
+        free(cli_system_id);
+    }
     service_stop();
 
     exit(retval);
@@ -5000,6 +5003,7 @@ parse_options(int argc, char *argv[])
         STREAM_SSL_LONG_OPTIONS,
         {"peer-ca-cert", required_argument, NULL, OPT_PEER_CA_CERT},
         {"bootstrap-ca-cert", required_argument, NULL, OPT_BOOTSTRAP_CA_CERT},
+        {"chassis", required_argument, NULL, 'n'},
         {"enable-dummy-vif-plug", no_argument, NULL,
          OPT_ENABLE_DUMMY_VIF_PLUG},
         {NULL, 0, NULL, 0}
@@ -5051,6 +5055,10 @@ parse_options(int argc, char *argv[])
             vif_plug_dummy_enable();
             break;
 
+        case 'n':
+            cli_system_id = xstrdup(optarg);
+            break;
+
         case '?':
             exit(EXIT_FAILURE);
 
@@ -5086,6 +5094,7 @@ usage(void)
     daemon_usage();
     vlog_usage();
     printf("\nOther options:\n"
+           "  -n                      custom chassis name\n"
            "  -h, --help              display this help message\n"
            "  -V, --version           display version information\n");
     exit(EXIT_SUCCESS);
