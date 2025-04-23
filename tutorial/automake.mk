93c0b4cb738a679e47c20e344469cdeba6779c60
Dumitru Ceara <dceara@redhat.com>
2025-04-23 13:27:52 +0200

    TODO: standalone podman frr l3 evpn setup

--- tutorial/automake.mk
+++ tutorial/automake.mk
@@ -4,7 +4,8 @@ EXTRA_DIST += \
 	tutorial/ovn-lb-benchmark.sh \
 	tutorial/ovn-lb-benchmark.py \
 	tutorial/evpn/Dockerfile \
-	tutorial/evpn/setup-l2.sh
+	tutorial/evpn/setup-l2.sh \
+	tutorial/evpn/setup-l3.sh
 sandbox: all
 	cd $(srcdir)/tutorial && MAKE=$(MAKE) HAVE_OPENSSL=$(HAVE_OPENSSL) \
 		./ovn-sandbox -b $(abs_builddir) --ovs-src $(ovs_srcdir) --ovs-build $(ovs_builddir) $(SANDBOXFLAGS)
