dceara/ovn

commit 3e53345ab6899617be3a94ff30b5e5f7636b5940

controller: Prevent assert with BGP when SB is read only.
There wasn't any check if SB is writable when we tried to commit
learned routes into SB, this would lead to assert and crash. In
order to prevent that track the SB state and add additional handler
that will trigger recompute of the route_exchange node if there are
SB changes pending and the SB is writable again.

Fixes: 866a5014ae45 ("controller: Support learning routes.")
Acked-by: Xavier Simonart <xsimonar@redhat.com>
Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
(cherry picked from commit 68c83ffbb9606681a13db85e272ded5fdb5dcf99)

controller/ovn-controller.c
@@ -5275,6 +5275,9 @@ route_sb_advertised_route_data_handler(struct engine_node *node, void *data)
 struct ed_type_route_exchange {
     /* We need the idl to check if the Learned_Route table exists. */
     struct ovsdb_idl *sb_idl;
+    /* Set to true when SB is readonly and we have routes that need
+     * to be inserted into SB. */
+    bool sb_changes_pending;
 };
 
 static void
@@ -5308,7 +5311,9 @@ en_route_exchange_run(struct engine_node *node, void *data)
         .sbrec_port_binding_by_name = sbrec_port_binding_by_name,
         .announce_routes = &route_data->announce_routes,
     };
-    struct route_exchange_ctx_out r_ctx_out = {};
+    struct route_exchange_ctx_out r_ctx_out = {
+        .sb_changes_pending = false,
+    };
 
     hmap_init(&r_ctx_out.route_table_watches);
 
@@ -5318,7 +5323,20 @@ en_route_exchange_run(struct engine_node *node, void *data)
     route_table_watch_request_cleanup(&r_ctx_out.route_table_watches);
     hmap_destroy(&r_ctx_out.route_table_watches);
 
+    re->sb_changes_pending = r_ctx_out.sb_changes_pending;
+
     engine_set_node_state(node, EN_UPDATED);
+}
+
+static bool
+route_exchange_sb_ro_handler(struct engine_node *node OVS_UNUSED, void *data)
+{
+    struct ed_type_route_exchange *re = data;
+    if (re->sb_changes_pending) {
+        return true;
+    }
+
+    return false;
 }
 
 
@@ -5748,6 +5766,8 @@ main(int argc, char *argv[])
                      engine_noop_handler);
     engine_add_input(&en_route_exchange, &en_route_table_notify, NULL);
     engine_add_input(&en_route_exchange, &en_route_exchange_status, NULL);
+    engine_add_input(&en_route_exchange, &en_sb_ro,
+                     route_exchange_sb_ro_handler);
 
     engine_add_input(&en_addr_sets, &en_sb_address_set,
                      addr_sets_sb_address_set_handler);

controller/route-exchange.c
@@ -137,7 +137,8 @@ sb_sync_learned_routes(const struct ovs_list *learned_routes,
                        const struct smap *bound_ports,
                        struct ovsdb_idl_txn *ovnsb_idl_txn,
                        struct ovsdb_idl_index *sbrec_port_binding_by_name,
-                       struct ovsdb_idl_index *sbrec_learned_route_by_datapath)
+                       struct ovsdb_idl_index *sbrec_learned_route_by_datapath,
+                       bool *sb_changes_pending)
 {
     struct hmap sync_routes = HMAP_INITIALIZER(&sync_routes);
     const struct sbrec_learned_route *sb_route;
@@ -186,6 +187,10 @@ sb_sync_learned_routes(const struct ovs_list *learned_routes,
                 hmap_remove(&sync_routes, &route_e->hmap_node);
                 free(route_e);
             } else {
+                if (!ovnsb_idl_txn) {
+                    *sb_changes_pending = true;
+                    continue;
+                }
                 sb_route = sbrec_learned_route_insert(ovnsb_idl_txn);
                 sbrec_learned_route_set_datapath(sb_route, datapath);
                 sbrec_learned_route_set_logical_port(sb_route, logical_port);
@@ -268,7 +273,8 @@ route_exchange_run(const struct route_exchange_ctx_in *r_ctx_in,
         sb_sync_learned_routes(&received_routes, ad->db,
                                &ad->bound_ports, r_ctx_in->ovnsb_idl_txn,
                                r_ctx_in->sbrec_port_binding_by_name,
-                               r_ctx_in->sbrec_learned_route_by_datapath);
+                               r_ctx_in->sbrec_learned_route_by_datapath,
+                               &r_ctx_out->sb_changes_pending);
 
         route_table_add_watch_request(&r_ctx_out->route_table_watches,
                                       table_id);

controller/route-exchange.h
@@ -31,6 +31,7 @@ struct route_exchange_ctx_in {
 
 struct route_exchange_ctx_out {
     struct hmap route_table_watches;
+    bool sb_changes_pending;
 };
 
 void route_exchange_run(const struct route_exchange_ctx_in *,

commit 7cbe89c26c209226317bd88fbf9dac277c9b43ca

controller: Fix bad backport of route_exchange_sb_ro_handler().
When backporting 68c83ffbb960 ("controller: Prevent assert with BGP
when SB is read only.") which added the new route_exchange handler for
en_sb_ro (SB readonly state) changes I messed up the return values.

On the main branch I-P handlers return EN_UNHANDLED if they
successfully processed input changes and EN_HANDLED_UNCHANGED
otherwise.

On older branches (25.03 included) they should return "true" if they
successfully processed input changes and "false" otherwise.  I mixed
these up and this commit addresses that.

Fixes: 3e53345ab689 ("controller: Prevent assert with BGP when SB is read only.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

controller/ovn-controller.c
@@ -5333,10 +5333,10 @@ route_exchange_sb_ro_handler(struct engine_node *node OVS_UNUSED, void *data)
 {
     struct ed_type_route_exchange *re = data;
     if (re->sb_changes_pending) {
-        return true;
+        return false;
     }
 
-    return false;
+    return true;
 }
 
 