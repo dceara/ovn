dceara/ovn

commit 0ccaea177420a11aa25ce8114b984ffbc1573c56

northd: Remove learned routes if lrp is removed.
Learned routes must be bound to a lrp on which the routes where learned.
In case the lrp is deleted for whatever reason no ovn-controller would
clean these routes up, therefor we do this in northd.

Acked-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Felix Huettner <felix.huettner@stackit.cloud>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

northd/en-learned-route-sync.c
@@ -193,7 +193,12 @@ routes_table_sync(
     struct hmap *parsed_routes_out)
 {
     const struct sbrec_learned_route *sb_route;
-    SBREC_LEARNED_ROUTE_TABLE_FOR_EACH (sb_route, sbrec_learned_route_table) {
+    SBREC_LEARNED_ROUTE_TABLE_FOR_EACH_SAFE (sb_route,
+                                             sbrec_learned_route_table) {
+        if (!ovn_port_find(lr_ports, sb_route->logical_port->logical_port)) {
+            sbrec_learned_route_delete(sb_route);
+            continue;
+        }
         parse_route_from_sbrec_route(parsed_routes_out, lr_ports,
                                      &lr_datapaths->datapaths,
                                      sb_route);

tests/ovn-northd.at
@@ -15195,6 +15195,22 @@ AT_CHECK([grep -w "lr_in_ip_routing" lr0flows | ovn_strip_lflows], [0], [dnl
   table=??(lr_in_ip_routing   ), priority=518  , match=(ip6.dst == 2001:db8:ffff::/64), action=(ip.ttl--; reg8[[0..15]] = 0; xxreg0 = ip6.dst; xxreg1 = 2001:db8:ffff::1; eth.src = 00:00:00:00:ff:02; outport = "lr0-sw1"; flags.loopback = 1; reg9[[9]] = 0; next;)
 ])
 
+# Deleting lr0-sw1 will remove the flows and also the learned route.
+check ovn-nbctl --wait=sb lrp-del lr0-sw1
+check_row_count Advertised_Route 2
+check_row_count Learned_Route 2
+check_row_count Learned_Route 2 logical_port=$sw0
+ovn-sbctl dump-flows lr0 > lr0flows
+AT_CHECK([grep -w "lr_in_ip_routing" lr0flows | ovn_strip_lflows], [0], [dnl
+  table=??(lr_in_ip_routing   ), priority=0    , match=(1), action=(drop;)
+  table=??(lr_in_ip_routing   ), priority=10550, match=(nd_rs || nd_ra), action=(drop;)
+  table=??(lr_in_ip_routing   ), priority=194  , match=(reg7 == 0 && ip4.dst == 172.16.0.0/24), action=(ip.ttl--; reg8[[0..15]] = 0; reg0 = 10.0.0.11; reg5 = 10.0.0.1; eth.src = 00:00:00:00:ff:01; outport = "lr0-sw0"; flags.loopback = 1; reg9[[9]] = 1; next;)
+  table=??(lr_in_ip_routing   ), priority=196  , match=(reg7 == 0 && ip4.dst == 192.168.0.0/24), action=(ip.ttl--; reg8[[0..15]] = 0; reg0 = 10.0.0.10; reg5 = 10.0.0.1; eth.src = 00:00:00:00:ff:01; outport = "lr0-sw0"; flags.loopback = 1; reg9[[9]] = 1; next;)
+  table=??(lr_in_ip_routing   ), priority=198  , match=(ip4.dst == 10.0.0.0/24), action=(ip.ttl--; reg8[[0..15]] = 0; reg0 = ip4.dst; reg5 = 10.0.0.1; eth.src = 00:00:00:00:ff:01; outport = "lr0-sw0"; flags.loopback = 1; reg9[[9]] = 1; next;)
+  table=??(lr_in_ip_routing   ), priority=514  , match=(reg7 == 0 && ip6.dst == 2001:db8:3::/64), action=(ip.ttl--; reg8[[0..15]] = 0; reg0 = 10.0.0.20; reg5 = 10.0.0.1; eth.src = 00:00:00:00:ff:01; outport = "lr0-sw0"; flags.loopback = 1; reg9[[9]] = 1; next;)
+  table=??(lr_in_ip_routing   ), priority=518  , match=(inport == "lr0-sw0" && ip6.dst == fe80::/64), action=(ip.ttl--; reg8[[0..15]] = 0; xxreg0 = ip6.dst; xxreg1 = fe80::200:ff:fe00:ff01; eth.src = 00:00:00:00:ff:01; outport = "lr0-sw0"; flags.loopback = 1; reg9[[9]] = 0; next;)
+])
+
 AT_CLEANUP
 ])
 

northd/northd.c
@@ -1369,6 +1369,12 @@ ovn_port_find(const struct hmap *ports, const char *name)
     return ovn_port_find__(ports, name, false);
 }
 
+static struct ovn_port *
+ovn_port_find_bound(const struct hmap *ports, const char *name)
+{
+    return ovn_port_find__(ports, name, true);
+}
+
 static bool
 lsp_is_clone_to_unknown(const struct nbrec_logical_switch_port *nbsp)
 {
@@ -1381,12 +1387,6 @@ lsp_is_clone_to_unknown(const struct nbrec_logical_switch_port *nbsp)
         }
     }
     return false;
-}
-
-static struct ovn_port *
-ovn_port_find_bound(const struct hmap *ports, const char *name)
-{
-    return ovn_port_find__(ports, name, true);
 }
 
 /* Returns true only if the logical switch port 'up' column is set to true.

commit d2e99f6d22018928e7d34b811c42df0baffe7ede

en-learned-route-sync: Fix Learned_Route cleanup for removed router ports.
It's not enough to check that ovn_port_find() returns non-NULL, that
also happens when in the current iteration the router (and router port)
have been deleted and new ones were added (with the same router port
name).  However, the Learned_Route was learned by ovn-controller for the
old Port_Binding and is potentially not applicable anymore.

Remove it by making sure we also match on the actual
Learned_Route.logical_port Port_Binding pointer being the correct one
when determining if a dynamic route is valid.

Otherwise we end up with an inconsistent SB database transaction that
fails integrity checks on the server side.

Fixes: 0ccaea177420 ("northd: Remove learned routes if lrp is removed.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

northd/en-learned-route-sync.c
@@ -220,7 +220,9 @@ routes_table_sync(
     const struct sbrec_learned_route *sb_route;
     SBREC_LEARNED_ROUTE_TABLE_FOR_EACH_SAFE (sb_route,
                                              sbrec_learned_route_table) {
-        if (!ovn_port_find(lr_ports, sb_route->logical_port->logical_port)) {
+        struct ovn_port *op =
+            ovn_port_find(lr_ports, sb_route->logical_port->logical_port);
+        if (!op || op->sb != sb_route->logical_port) {
             sbrec_learned_route_delete(sb_route);
             continue;
         }

tests/ovn-northd.at
@@ -15411,6 +15411,33 @@ AT_CLEANUP
 ])
 
 OVN_FOR_EACH_NORTHD_NO_HV([
+AT_SETUP([dynamic-routing - route learning cleanup - router recreation])
+AT_KEYWORDS([dynamic-routing])
+ovn_start
+
+check ovn-nbctl --wait=sb                             \
+    -- lr-add lr                                      \
+       -- lrp-add lr lrp 00:00:00:00:00:01 1.1.1.1/24
+
+lr_dp=$(fetch_column Datapath_Binding _uuid external_ids:name=lr)
+lrp_pb=$(fetch_column Port_Binding _uuid logical_port=lrp)
+check_uuid ovn-sbctl create Learned_Route       \
+    datapath=$lr_dp logical_port=$lrp_pb        \
+    ip_prefix=42.42.42.42/32 nexthop=1.1.1.2/24
+
+dnl Delete old router, create a new one with the same name, learned routes
+dnl should be removed.
+check ovn-nbctl --wait=sb                             \
+    -- lrp-del lrp                                    \
+    -- lr-del lr                                      \
+    -- lr-add lr                                      \
+       -- lrp-add lr lrp 00:00:00:00:00:01 1.1.1.1/24
+check_row_count Learned_Route 0
+
+AT_CLEANUP
+])
+
+OVN_FOR_EACH_NORTHD_NO_HV([
 AT_SETUP([dynamic-routing - host routes])
 AT_KEYWORDS([dynamic-routing])
 ovn_start