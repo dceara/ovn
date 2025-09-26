a7e2fd99591c2b3d603395d2e05ef1ab5615162e
Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
2025-09-26 18:01:56 +0200

    northd: Do not sample twice on the same logical-switch connection tracking zone.
    Introduce REGBIT_PKT_SAMPLED (reg0[22]) bit in the logical switch
    pipeline in order to avoid sampling traffic hitting the ACLs multiple
    times (e.g. when the traffic is egressing from the patch port connecting
    the logical switch to a logical router).
    Preserve the REGBIT_PKT_SAMPLED value passing from the logical switch
    ingress pipeline to  the logical switch egress one.
    This patch will fix the oversampling issue reported in
    https://mail.openvswitch.org/pipermail/ovs-discuss/2025-May/053626.html
    
    Reported-at: https://issues.redhat.com/browse/FDP-1408
    Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>

--- northd/northd.c
+++ northd/northd.c
@@ -135,6 +135,7 @@ static bool vxlan_mode;
 #define REGBIT_IP_FRAG            "reg0[19]"
 #define REGBIT_ACL_PERSIST_ID     "reg0[20]"
 #define REGBIT_ACL_HINT_ALLOW_PERSISTED "reg0[21]"
+#define REGBIT_PKT_SAMPLED        "reg0[22]"
 
 /* Register definitions for switches and routers. */
 
@@ -5921,7 +5922,7 @@ skip_port_from_conntrack(const struct ovn_datapath *od, struct ovn_port *op,
     const char *ingress_action = "next;";
     const char *egress_action = has_stateful_acl
                                 ? "next;"
-                                : "ct_clear; next;";
+                                : REGBIT_PKT_SAMPLED" = 0; ct_clear; next;";
 
     char *ingress_match = xasprintf("ip && inport == %s", op->json_key);
     char *egress_match = xasprintf("ip && outport == %s", op->json_key);
@@ -6535,7 +6536,8 @@ build_acl_sample_action(struct ds *actions, const struct nbrec_acl *acl,
     }
 
     for (size_t i = 0; i < sample->n_collectors; i++) {
-        ds_put_format(actions, "sample(probability=%"PRIu16","
+        ds_put_format(actions, REGBIT_PKT_SAMPLED" = 1; "
+                               "sample(probability=%"PRIu16","
                                "collector_set=%"PRIu32","
                                "obs_domain=%"PRIu32","
                                "obs_point=%"PRIu32");",
@@ -6693,7 +6695,8 @@ build_acl_sample_est_orig_stateful_flows(const struct ovn_datapath *od,
     ds_clear(actions);
     ds_clear(match);
 
-    ds_put_cstr(match, "ip && ct.trk && "
+    ds_put_cstr(match, REGBIT_PKT_SAMPLED" == 0 && "
+                       "ip && ct.trk && "
                        "(ct.est || ct.rel) && "
                        "!ct.rpl && ");
     build_acl_sample_label_match(match, acl, acl->sample_est);
@@ -6726,9 +6729,10 @@ build_acl_sample_est_rpl_stateful_flows(const struct ovn_datapath *od,
     ds_clear(actions);
     ds_clear(match);
 
-    ds_put_cstr(match, "ip && ct.trk && "
-                        "(ct.est || ct.rel) && "
-                        "ct.rpl && ");
+    ds_put_cstr(match, REGBIT_PKT_SAMPLED" == 0 && "
+                       "ip && ct.trk && "
+                       "(ct.est || ct.rel) && "
+                       "ct.rpl && ");
     build_acl_sample_label_match(match, acl, acl->sample_est);
 
     build_acl_sample_action(actions, acl, acl->sample_est, sample_domain_id);
@@ -6803,7 +6807,8 @@ build_acl_sample_generic_new_flows(const struct ovn_datapath *od,
                          (uint8_t) coll->id,
                          (uint8_t) obs_stage);
 
-    ds_put_format(actions, "sample(probability=%"PRIu16","
+    ds_put_format(actions, REGBIT_PKT_SAMPLED" = 1; "
+                           "sample(probability=%"PRIu16","
                            "collector_set=%"PRIu32","
                            "obs_domain=%"PRIu32","
                            "obs_point="REG_OBS_POINT_ID_NEW");"
@@ -6833,7 +6838,8 @@ build_acl_sample_generic_est_flows(const struct ovn_datapath *od,
     ds_clear(match);
     ds_clear(actions);
 
-    ds_put_cstr(match, "ip && ct.trk && (ct.est || ct.rel) && "
+    ds_put_cstr(match, REGBIT_PKT_SAMPLED" == 0 && "
+                       "ip && ct.trk && (ct.est || ct.rel) && "
                        "ct_label.obs_unused == 0 && ");
 
     size_t match_len = match->length;
@@ -6842,7 +6848,8 @@ build_acl_sample_generic_est_flows(const struct ovn_datapath *od,
                          (uint8_t) coll->id,
                          (uint8_t) obs_stage);
 
-    ds_put_format(actions, "sample(probability=%"PRIu16","
+    ds_put_format(actions, REGBIT_PKT_SAMPLED" = 1; "
+                           "sample(probability=%"PRIu16","
                            "collector_set=%"PRIu32","
                            "obs_domain=%"PRIu32","
                            "obs_point=ct_label.obs_point_id);"
