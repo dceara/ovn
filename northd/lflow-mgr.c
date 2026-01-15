dceara/ovn

commit fd4762a8c9c30d179e848b74c49dd4afac6c8f03

lflow-mgr: Use an array for lflow dp groups.
The lflow_table had separate ls and lr datapath groups as individual
pointers. If we expand to allow new types of datapaths, then this is
overly restrictive. Using an array allows us to automatically expand as
new datapath types are added.

Signed-off-by: Mark Michelson <mmichels@redhat.com>
Acked-by: Ales Musil <amusil@redhat.com>
Acked-by: Numan Siddique <numans@ovn.org>

northd/lflow-mgr.c
@@ -207,8 +207,9 @@ lflow_table_init(struct lflow_table *lflow_table)
 {
     fast_hmap_size_for(&lflow_table->entries,
                        lflow_table->max_seen_lflow_size);
-    ovn_dp_groups_init(&lflow_table->ls_dp_groups);
-    ovn_dp_groups_init(&lflow_table->lr_dp_groups);
+    for (enum ovn_datapath_type i = DP_MIN; i < DP_MAX; i++) {
+        ovn_dp_groups_init(&lflow_table->dp_groups[i]);
+    }
 }
 
 void
@@ -223,8 +224,9 @@ lflow_table_clear(struct lflow_table *lflow_table, bool destroy_all)
         }
     }
 
-    ovn_dp_groups_clear(&lflow_table->ls_dp_groups);
-    ovn_dp_groups_clear(&lflow_table->lr_dp_groups);
+    for (enum ovn_datapath_type i = DP_MIN; i < DP_MAX; i++) {
+        ovn_dp_groups_clear(&lflow_table->dp_groups[i]);
+    }
 }
 
 void
@@ -232,8 +234,9 @@ lflow_table_destroy(struct lflow_table *lflow_table)
 {
     lflow_table_clear(lflow_table, true);
     hmap_destroy(&lflow_table->entries);
-    ovn_dp_groups_destroy(&lflow_table->ls_dp_groups);
-    ovn_dp_groups_destroy(&lflow_table->lr_dp_groups);
+    for (enum ovn_datapath_type i = DP_MIN; i < DP_MAX; i++) {
+        ovn_dp_groups_destroy(&lflow_table->dp_groups[i]);
+    }
     free(lflow_table);
 }
 
@@ -288,12 +291,13 @@ lflow_table_sync_to_sb(struct lflow_table *lflow_table,
         }
         const struct ovn_datapaths *datapaths;
         struct hmap *dp_groups;
-        if (ovn_stage_to_datapath_type(lflow->stage) == DP_SWITCH) {
+        enum ovn_datapath_type dp_type =
+            ovn_stage_to_datapath_type(lflow->stage);
+        dp_groups = &lflow_table->dp_groups[dp_type];
+        if (dp_type == DP_SWITCH) {
             datapaths = ls_datapaths;
-            dp_groups = &lflow_table->ls_dp_groups;
         } else {
             datapaths = lr_datapaths;
-            dp_groups = &lflow_table->lr_dp_groups;
         }
         sync_lflow_to_sb(lflow, ovnsb_txn, dp_groups, datapaths,
                          ovn_internal_version_changed,
@@ -366,12 +370,11 @@ lflow_table_sync_to_sb(struct lflow_table *lflow_table,
         if (lflow) {
             const struct ovn_datapaths *datapaths;
             struct hmap *dp_groups;
+            dp_groups = &lflow_table->dp_groups[dp_type];
             if (dp_type == DP_SWITCH) {
                 datapaths = ls_datapaths;
-                dp_groups = &lflow_table->ls_dp_groups;
             } else {
                 datapaths = lr_datapaths;
-                dp_groups = &lflow_table->lr_dp_groups;
             }
             sync_lflow_to_sb(lflow, ovnsb_txn, dp_groups, datapaths,
                              ovn_internal_version_changed,
@@ -391,12 +394,13 @@ lflow_table_sync_to_sb(struct lflow_table *lflow_table,
         }
         const struct ovn_datapaths *datapaths;
         struct hmap *dp_groups;
-        if (ovn_stage_to_datapath_type(lflow->stage) == DP_SWITCH) {
+        enum ovn_datapath_type dp_type =
+            ovn_stage_to_datapath_type(lflow->stage);
+        dp_groups = &lflow_table->dp_groups[dp_type];
+        if (dp_type == DP_SWITCH) {
             datapaths = ls_datapaths;
-            dp_groups = &lflow_table->ls_dp_groups;
         } else {
             datapaths = lr_datapaths;
-            dp_groups = &lflow_table->lr_dp_groups;
         }
         sync_lflow_to_sb(lflow, ovnsb_txn, dp_groups, datapaths,
                          ovn_internal_version_changed, NULL, dpgrp_table);
@@ -1358,11 +1362,12 @@ lflow_ref_sync_lflows__(struct lflow_ref  *lflow_ref,
 
         struct hmap *dp_groups = NULL;
         const struct ovn_datapaths *datapaths;
-        if (ovn_stage_to_datapath_type(lflow->stage) == DP_SWITCH) {
-            dp_groups = &lflow_table->ls_dp_groups;
+        enum ovn_datapath_type dp_type =
+            ovn_stage_to_datapath_type(lflow->stage);
+        dp_groups = &lflow_table->dp_groups[dp_type];
+        if (dp_type == DP_SWITCH) {
             datapaths = ls_datapaths;
         } else {
-            dp_groups = &lflow_table->lr_dp_groups;
             datapaths = lr_datapaths;
         }
 

northd/lflow-mgr.h
@@ -28,8 +28,7 @@ struct ovsdb_idl_row;
 /* lflow map which stores the logical flows. */
 struct lflow_table {
     struct hmap entries; /* hmap of lflows. */
-    struct hmap ls_dp_groups; /* hmap of logical switch dp groups. */
-    struct hmap lr_dp_groups; /* hmap of logical router dp groups. */
+    struct hmap dp_groups[DP_MAX];
     ssize_t max_seen_lflow_size;
 };
 

commit 603c4dd6991f329f813c57aa204e65b818682451

lflow-mgr: Make it explicit that dp_type cannot be unknown.
Coverity complained that there's a chance that
ovn_datapath_type_from_string() returns a value >= DP_MAX in
lflow_table_sync_to_sb().

That's actually not possible, the report is a false positive, but to
make it explicit add an assert (as we do in other places that work on
datapaths that have been sanitized).

Coverity report:
 CID 501213: (#1 of 1): Out-of-bounds read (OVERRUN)
 overrun-local: Overrunning array of 336 bytes at byte offset
 336 by dereferencing pointer &dps[dp_type].

Fixes: fd4762a8c9c3 ("lflow-mgr: Use an array for lflow dp groups.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

northd/lflow-mgr.c
@@ -292,6 +292,8 @@ lflow_table_sync_to_sb(struct lflow_table *lflow_table,
         struct hmap *dp_groups;
         enum ovn_datapath_type dp_type =
             ovn_stage_to_datapath_type(lflow->stage);
+        ovs_assert(dp_type < DP_MAX);
+
         dp_groups = &lflow_table->dp_groups[dp_type];
         datapaths = &dps[dp_type];
         sync_lflow_to_sb(lflow, ovnsb_txn, dp_groups, datapaths,
@@ -323,12 +325,16 @@ lflow_table_sync_to_sb(struct lflow_table *lflow_table,
         if (dp) {
             enum ovn_datapath_type dp_type =
                 ovn_datapath_type_from_string(datapath_get_nb_type(dp));
+            ovs_assert(dp_type < DP_MAX);
+
             sdp = ovn_synced_datapath_from_sb(&dps[dp_type], dp);
         }
         for (i = 0; dp_group && i < dp_group->n_datapaths; i++) {
             enum ovn_datapath_type dp_type =
                 ovn_datapath_type_from_string(datapath_get_nb_type(
                     dp_group->datapaths[i]));
+            ovs_assert(dp_type < DP_MAX);
+
             sdp = ovn_synced_datapath_from_sb(&dps[dp_type],
                                               dp_group->datapaths[i]);
             if (sdp) {