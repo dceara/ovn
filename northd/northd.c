b98d28394296ccef8356b13c006248cb8650c0c7
Dumitru Ceara <dceara@redhat.com>
2026-03-03 12:19:57 +0100

    TODO: Use MC_UNKNOWN for broadcast ARP requests.
    JUST FOR TESTING.
    
    Reported-at: https://mail.openvswitch.org/pipermail/ovs-discuss/2026-March/053932.html
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>

--- northd/northd.c
+++ northd/northd.c
@@ -8981,7 +8981,7 @@ build_lswitch_rport_arp_req_flow(
      */
     if (od->n_router_ports != od->nbs->n_ports) {
         ds_put_format(&actions, "clone {outport = %s; output; }; "
-                                "outport = \""MC_FLOOD_L2"\"; output;",
+                                "outport = \""MC_UNKNOWN"\"; output;",
                       patch_op->json_key);
         ovn_lflow_add_with_hint(lflows, od, S_SWITCH_IN_L2_LKUP,
                                 priority, ds_cstr(&match),
@@ -9003,7 +9003,7 @@ build_lswitch_rport_arp_req_flow(
         ds_clear(&actions);
         if (od->n_router_ports != od->nbs->n_ports) {
             ds_put_format(&actions, "clone {outport = %s; output; }; "
-                                    "outport = \""MC_FLOOD_L2"\"; output;",
+                                    "outport = \""MC_UNKNOWN"\"; output;",
                           patch_op->cr_port->json_key);
             ovn_lflow_add_with_hint(lflows, od, S_SWITCH_IN_L2_LKUP,
                                     priority, ds_cstr(&match),
@@ -10336,14 +10336,16 @@ build_lswitch_destination_lookup_bmcast(struct ovn_datapath *od,
         }
     }
 
+    //TODO: make this depend on the mac binding aging timeout config?
     if (!smap_get_bool(&od->nbs->other_config,
                        "broadcast-arps-to-all-routers", true)) {
         ovn_lflow_add(lflows, od, S_SWITCH_IN_L2_LKUP, 72,
                       "eth.mcast && (arp.op == 1 || nd_ns)",
-                      "outport = \""MC_FLOOD_L2"\"; output;",
+                      "outport = \""MC_UNKNOWN"\"; output;",
                       lflow_ref);
     }
 
+    //TODO: make this depend on the mac binding aging timeout config?
     ovn_lflow_add(lflows, od, S_SWITCH_IN_L2_LKUP, 71,
                   "eth.mcast && (arp || ip)",
                   "outport = \""MC_FLOOD"\"; output;", lflow_ref);
