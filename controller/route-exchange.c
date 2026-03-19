dceara/ovn

commit 2ddbb21ac699349c4a6122cd7bbef291a7d69ac2

controller, northd: Add option to disable route learning.
Add "dynamic-routing-no-learning" option to router and router port options.
This option will disable learning on a router/router port and remove
existing learned routes. The port option takes priority over the router
option.

Reported-at: https://issues.redhat.com/browse/FDP-2750
Signed-off-by: Mairtin O'Loingsigh <moloings@redhat.com>
Signed-off-by: Ales Musil <amusil@redhat.com>

controller/route-exchange.c
@@ -187,6 +187,14 @@ sb_sync_learned_routes(const struct vector *learned_routes,
             if (!logical_port) {
                 continue;
             }
+
+            bool no_learning = smap_get_bool(&logical_port->options,
+                                             "dynamic-routing-no-learning",
+                                             false);
+            if (no_learning) {
+                continue;
+            }
+
             route_e = route_lookup(&sync_routes, datapath,
                                    logical_port, ip_prefix, nexthop);
             if (route_e) {

northd/northd.c
@@ -3919,6 +3919,15 @@ sync_pb_for_lrp(struct ovn_port *op,
         if (redistribute_local_only_val) {
             smap_add(&new, redistribute_local_only_name, "true");
         }
+
+        /* Set no-learning on ports based on NB router/router port config */
+        const char *no_learn_name = "dynamic-routing-no-learning";
+        bool no_learning = smap_get_bool(&op->nbrp->options, no_learn_name,
+                                         smap_get_bool(&op->od->nbr->options,
+                                                       no_learn_name, false));
+        if (no_learning) {
+            smap_add(&new, "dynamic-routing-no-learning", "true");
+        }
     }
 
     const char *ipv6_pd_list = smap_get(&op->sb->options, "ipv6_ra_pd_list");

ovn-nb.xml
@@ -3442,6 +3442,19 @@ or
         </p>
       </column>
 
+      <column name="options" key="dynamic-routing-no-learning"
+              type='{"type": "boolean"}'>
+        <p>
+          Only relevant if <ref column="options" key="dynamic-routing"/>
+          is set to <code>true</code>.
+        </p>
+
+        <p>
+          This option disables route learning on a specific router and will
+          also remove learned routes.
+        </p>
+      </column>
+
       <column name="options" key="dynamic-routing-v4-prefix-nexthop"
               type='{"type": "string"}'>
         <p>
@@ -4580,6 +4593,20 @@ or
 
           This allows to filter CIDR prefixes in the process of learning
           routes in <code>ovn-ic</code> daemon.
+        </p>
+      </column>
+
+      <column name="options" key="dynamic-routing-no-learning"
+              type='{"type": "boolean"}'>
+        <p>
+          Only relevant if <ref column="options" key="dynamic-routing"/>
+          is set to <code>true</code>.
+        </p>
+
+        <p>
+          This option disables route learning on a specific router port and
+          will also remove learned routes. It also has priority over the
+          router version of this option.
         </p>
       </column>
     </group>

ovn-sb.xml
@@ -3925,6 +3925,21 @@ tcp.flags = RST;
           bound.  Default: <code>false</code>.
         </p>
       </column>
+
+      <column name="options" key="dynamic-routing-no-learning"
+              type='{"type": "boolean"}'>
+        <p>
+          Only relevant if <ref column="options" key="dynamic-routing"/>
+          is set to <code>true</code>.
+        </p>
+
+        <p>
+          This option disables adding routes to
+          <ref table="Learned_Route" db="OVN_Southbound"/> and will also
+          remove learned routes.
+        </p>
+      </column>
+
     </group>
 
     <group title="Nested Containers">

tests/system-ovn.at
@@ -19954,15 +19954,98 @@ AT_CHECK([ip route del 10.10.3.1 via 20.0.0.25 vrf vrf-$vni])
 OVS_WAIT_FOR_OUTPUT([ovn-sbctl list Learned_Route | grep ip_prefix | sort], [0], [dnl
 ])
 
+# Disable learning on router
+AS_BOX([$(date +%H:%M:%S.%03N) Disable dynamic-route learning])
+
+# Add a route to the VRF (simulating BGP learning a route)
+AT_CHECK([ip route add 10.10.3.1 via 20.0.0.25 vrf vrf-$vni proto zebra])
+
+# Verify learned route appears in SB database
+wait_row_count Learned_Route 1 ip_prefix=10.10.3.1
+
+check ovn-nbctl --wait=hv set Logical_Router lr-frr options:dynamic-routing-no-learning=true
+
+# Verify routes do not appear in SB database.
+wait_row_count Learned_Route 0
+
+check ovn-nbctl --wait=hv set Logical_Router lr-frr options:dynamic-routing-no-learning=false
+
+# Verify learned route appears in SB database
+wait_row_count Learned_Route 1 ip_prefix=10.10.3.1
+
+AT_CHECK([ip route del 10.10.3.1 via 20.0.0.25 vrf vrf-$vni])
+
+# Verify all routes removed from SB database.
+wait_row_count Learned_Route 0
+
+# Disable learning on router then attempt to add route
+AS_BOX([$(date +%H:%M:%S.%03N) Disable dynamic-route learning 2])
+
+check ovn-nbctl --wait=hv set Logical_Router lr-frr options:dynamic-routing-no-learning=true
+
+# Add a route to the VRF (simulating BGP learning a route)
+AT_CHECK([ip route add 10.10.3.1 via 20.0.0.25 vrf vrf-$vni proto zebra])
+
+# Verify routes do not appear in SB database.
+wait_row_count Learned_Route 0
+
+check ovn-nbctl --wait=hv set Logical_Router lr-frr options:dynamic-routing-no-learning=false
+
+# Verify learned route appears in SB database
+wait_row_count Learned_Route 1 ip_prefix=10.10.3.1
+
+AT_CHECK([ip route del 10.10.3.1 via 20.0.0.25 vrf vrf-$vni])
+
+# Verify all routes removed from SB database.
+wait_row_count Learned_Route 0
+
+# Disable learning on router port
+AS_BOX([$(date +%H:%M:%S.%03N) Disable dynamic-route learning on port])
+check ovn-nbctl --wait=hv set Logical_Router_Port lrp-local-bgp-port options:dynamic-routing-no-learning=true
+
+# Add a route to the VRF (simulating BGP learning a route)
+AT_CHECK([ip route add 10.10.3.1 via 20.0.0.25 vrf vrf-$vni proto zebra])
+
+# Verify routes do not appear in SB database.
+wait_row_count Learned_Route 0
+
+AT_CHECK([ip route del 10.10.3.1 via 20.0.0.25 vrf vrf-$vni])
+check ovn-nbctl --wait=hv set Logical_Router_Port lrp-local-bgp-port options:dynamic-routing-no-learning=false
+
 # Add again a route to the VRF (simulating BGP learning a route)
 AT_CHECK([ip route add 10.10.3.1 via 20.0.0.25 vrf vrf-$vni proto zebra])
 
 # Verify learned route appears in SB database
-OVS_WAIT_UNTIL([ovn-sbctl list Learned_Route | grep ip_prefix | grep -Fe 10.10.3.1])
+wait_row_count Learned_Route 1 ip_prefix=10.10.3.1
+
+AT_CHECK([ip route del 10.10.3.1 via 20.0.0.25 vrf vrf-$vni])
+
+# Verify all routes removed from SB database.
+wait_row_count Learned_Route 0
+
+# Disable learning on router and enable on port
+AS_BOX([$(date +%H:%M:%S.%03N) Disable dynamic-route learning on router, but not port])
+check ovn-nbctl --wait=hv set Logical_Router lr-frr options:dynamic-routing-no-learning=true
+check ovn-nbctl --wait=hv set Logical_Router_Port lrp-local-bgp-port options:dynamic-routing-no-learning=false
+
+# Add a route to the VRF (simulating BGP learning a route)
+AT_CHECK([ip route add 10.10.3.1 via 20.0.0.25 vrf vrf-$vni proto zebra])
+
+# Verify routes do not appear in SB database.
+wait_row_count Learned_Route 1
+
+AT_CHECK([ip route del 10.10.3.1 via 20.0.0.25 vrf vrf-$vni])
+check ovn-nbctl --wait=hv set Logical_Router lr-frr options:dynamic-routing-no-learning=false
+
+# Add again a route to the VRF (simulating BGP learning a route)
+AT_CHECK([ip route add 10.10.3.1 via 20.0.0.25 vrf vrf-$vni proto zebra])
+
+# Verify learned route appears in SB database
+wait_row_count Learned_Route 1 ip_prefix=10.10.3.1
 
 # Disable dynamic routing
 AS_BOX([$(date +%H:%M:%S.%03N) Disable dynamic-routing])
-check ovn-nbctl --wait=sb set Logical_Router lr-frr options:dynamic-routing=false
+check ovn-nbctl --wait=hv set Logical_Router lr-frr options:dynamic-routing=false
 check_row_count Port_Binding 0 logical_port=lrp-local-bgp-port 'options:dynamic-routing=true'
 
 # Add one more route to the VRF (simulating BGP learning a route)
@@ -19973,7 +20056,7 @@ OVS_WAIT_FOR_OUTPUT([ovn-sbctl list Learned_Route | grep ip_prefix | sort], [0],
 ])
 
 # Re-enable dynamic routing
-check ovn-nbctl --wait=sb set Logical_Router lr-frr options:dynamic-routing=true
+check ovn-nbctl --wait=hv set Logical_Router lr-frr options:dynamic-routing=true
 check_row_count Port_Binding 1 logical_port=lrp-local-bgp-port 'options:dynamic-routing=true'
 
 # Verify both routes appear in SB database.

commit a13d86c245a20dbb91b7580c0a690b665b79fca2

TODO: make no-learning dynamic-routing actually work!
Fixes: 2ddbb21ac699 ("controller, northd: Add option to disable route learning.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

controller/route-exchange.c
@@ -267,7 +267,12 @@ route_exchange_run(const struct route_exchange_ctx_in *r_ctx_in,
                          "%s table id: %"PRIu32,
                          UUID_ARGS(&ad->db->header_.uuid),
                          !valid ? "invalid" : "duplicate", table_id);
-            continue;
+            //TODO: we need a better fix probably:
+            //don't skip dups, we might have "no-learning=true" but we still
+            //need to monitor in order to advertise
+            if (!valid) {
+                continue;
+            }
         }
 
         if (ad->maintain_vrf) {