dceara/ovn

commit c73f7912665d6a6e935c7c8aeae8389c059e9ddb

northd: Ensure unicast ARPs are forwarded properly.
By default, northd does not install any responder flows for unicast ARP.
These are intended to be forwarded to the destination so that the
destination can respond appropriately. For VIF ports, this tends to work
as expected in most scenarios.

When proxy ARP is configured on a logical switch port connected to a
logical router, then we install low priority flows to ensure that
ARPs for the configured proxy addresses are responded to by the logical
switch. These proxy ARP flows are hit when unicast ARP requests are sent
for the VIF ports on the logical switch. We therefore end up responding
incorrectly to unicast ARP requests with the proxy ARP MAC instead of
forwarding the ARP request to the proper VIF port.

This commit fixes the issue by installing explicit "next;" actions for
unicast ARP requests directed towards VIFs so that the proxy ARP flows
will not be hit. These flows are only installed if proxy ARP is
configured, since they are unnecessary otherwise.

Reported-at: https://issues.redhat.com/browse/FDP-1646
Acked-by: Numan Siddique <numans@ovn.org>
Signed-off-by: Mark Michelson <mmichels@redhat.com>

northd/northd.c
@@ -9956,15 +9956,32 @@ build_lswitch_arp_nd_responder_known_ips(struct ovn_port *op,
         for (size_t i = 0; i < op->n_lsp_addrs; i++) {
             for (size_t j = 0; j < op->lsp_addrs[i].n_ipv4_addrs; j++) {
                 ds_clear(match);
-                /* Do not reply on unicast ARPs, forward them to the target
-                 * to have ability to monitor target liveness via unicast
-                 * ARP requests.
-                */
                 ds_put_format(match,
                     "arp.tpa == %s && "
-                    "arp.op == 1 && "
-                    "eth.dst == ff:ff:ff:ff:ff:ff",
-                    op->lsp_addrs[i].ipv4_addrs[j].addr_s);
+                    "arp.op == 1", op->lsp_addrs[i].ipv4_addrs[j].addr_s);
+
+                /* Do not reply on unicast ARPs, forward them to the target
+                 * to have ability to monitor target liveness via unicast
+                 * ARP requests. If proxy arp is configured, then we need
+                 * to set up flows to forward the packets. Otherwise, we
+                 * could end up replying with the proxy ARP erroneously.
+                 * Without proxy arp configured, these flows are
+                 * unnecessary since the packets will hit the default
+                 * "next" flow at priority 0.
+                 */
+                if (op->od->has_arp_proxy_port) {
+                    size_t match_len = match->length;
+                    ds_put_format(match, " && eth.dst == %s",
+                                  op->lsp_addrs[i].ea_s);
+                    ovn_lflow_add_with_hint(lflows, op->od,
+                                            S_SWITCH_IN_ARP_ND_RSP, 50,
+                                            ds_cstr(match),
+                                            "next;", &op->nbsp->header_,
+                                            op->lflow_ref);
+                    ds_truncate(match, match_len);
+                }
+                ds_put_cstr(match, " && eth.dst == ff:ff:ff:ff:ff:ff");
+
                 ds_clear(actions);
                 ds_put_format(actions,
                     "eth.dst = eth.src; "
@@ -10013,9 +10030,27 @@ build_lswitch_arp_nd_responder_known_ips(struct ovn_port *op,
              *
              *   - Do not reply for unicast ND solicitations.  Let the target
              *     reply to it, so that the sender has the ability to monitor
-             *     the target liveness via the unicast ND solicitations.
+             *     the target liveness via the unicast ND solicitations. Like
+             *     with IPv4, if proxy ARP is configured, then we need to
+             *     install unicast nd_ns flows that allow the packet to reach
+             *     its target as intended.
              */
             for (size_t j = 0; j < op->lsp_addrs[i].n_ipv6_addrs; j++) {
+                if (op->od->has_arp_proxy_port) {
+                    ds_clear(match);
+                    ds_put_format(match,
+                                  "nd_ns && ip6.dst == %s && nd.target == %s",
+                                  op->lsp_addrs[i].ipv6_addrs[j].addr_s,
+                                  op->lsp_addrs[i].ipv6_addrs[j].addr_s);
+                    ovn_lflow_add_with_hint__(lflows, op->od,
+                                              S_SWITCH_IN_ARP_ND_RSP, 50,
+                                              ds_cstr(match), "next;", NULL,
+                                              copp_meter_get(COPP_ND_NA,
+                                                             op->od->nbs->copp,
+                                                             meter_groups),
+                                              &op->nbsp->header_,
+                                              op->lflow_ref);
+                }
                 ds_clear(match);
                 ds_put_format(
                     match,

tests/ovn-northd.at
@@ -18716,3 +18716,82 @@ AT_CHECK(
 
 AT_CLEANUP
 ])
+
+OVN_FOR_EACH_NORTHD_NO_HV([
+AT_SETUP([Unicast ARP flows])
+ovn_start
+
+# Typically on logical switches, the ARP responder stage installs nothing
+# for unicast ARPs towards VIF MACs. Instead, we rely on the default priority
+# 1 "next;" action to move these ARPs through the pipeline, eventually resulting
+# in the ARP reaching the destination. When proxy ARP is configured, we need
+# to install explicit flows for the unicast ARPs at a higher priority. Otherwise
+# the proxy ARP responder may respond with incorrect information.
+#
+# In this test, we are ensuring that the unicast flows in the ARP responder stage
+# are only installed when proxy ARP is enabled.
+
+check ovn-nbctl ls-add ls1
+check ovn-nbctl lsp-add ls1 vm1 -- \
+      lsp-set-addresses vm1 "00:00:00:00:00:02 192.168.0.2 fd01::2"
+
+check ovn-nbctl lr-add lr1
+check ovn-nbctl lrp-add lr1 lr1-ls1 00:00:00:00:00:01 192.168.0.1 fd01::1
+check ovn-nbctl lsp-add ls1 ls1-lr1 -- \
+      lsp-set-addresses ls1-lr1 router -- \
+      lsp-set-type ls1-lr1 router -- \
+      lsp-set-options ls1-lr1 router-port=lr1-ls1
+
+check ovn-nbctl --wait=sb sync
+
+# If we check the ARP responder flows in ls1, we should not see any unicast
+# flows for vm1 (00:00:00:00:00:02). We also should not see any unicast
+# IPv6 ND flows for vm1 (fd01::2)
+AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep "eth.dst == 00:00:00:00:00:02" | ovn_strip_lflows], [0], [dnl
+])
+AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep "ip6.dst == fd01::2" | ovn_strip_lflows], [0], [dnl
+])
+
+AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep "eth.dst == 00:00:00:00:00:01" | ovn_strip_lflows], [0], [dnl
+])
+AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep "ip6.dst == fd01::1" | ovn_strip_lflows], [0], [dnl
+])
+
+# Add ARP proxy configuration on the router port.
+check ovn-nbctl set logical_switch_port ls1-lr1 options:arp_proxy="192.168.0.0/24 fd01::/64"
+check ovn-nbctl --wait=sb sync
+
+# Now that we have ARP proxy configured, we should see flows for
+# vm1's MAC.
+AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep "eth.dst == 00:00:00:00:00:02" | ovn_strip_lflows], [0], [dnl
+  table=??(ls_in_arp_rsp      ), priority=50   , match=(arp.tpa == 192.168.0.2 && arp.op == 1 && eth.dst == 00:00:00:00:00:02), action=(next;)
+])
+# And we should see unicast ND flows for vm1's IP address
+AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep "ip6.dst == fd01::2" | ovn_strip_lflows], [0], [dnl
+  table=??(ls_in_arp_rsp      ), priority=50   , match=(nd_ns && ip6.dst == fd01::2 && nd.target == fd01::2), action=(next;)
+])
+
+# We should also see an ARP flow for ls1-lr1's MAC.
+AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep "eth.dst == 00:00:00:00:00:01" | ovn_strip_lflows], [0], [dnl
+  table=??(ls_in_arp_rsp      ), priority=50   , match=(arp.tpa == 192.168.0.1 && arp.op == 1 && eth.dst == 00:00:00:00:00:01), action=(next;)
+])
+# And we should see an ND flow for ls1-lr1's IPv6 address.
+AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep "ip6.dst == fd01::1" | ovn_strip_lflows], [0], [dnl
+  table=??(ls_in_arp_rsp      ), priority=50   , match=(nd_ns && ip6.dst == fd01::1 && nd.target == fd01::1), action=(next;)
+])
+
+check ovn-nbctl remove logical_switch_port ls1-lr1 options arp_proxy
+check ovn-nbctl --wait=sb sync
+
+# We have removed ARP proxy, so we should no longer see a unicast ARP or ND flow.
+AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep "eth.dst == 00:00:00:00:00:02" | ovn_strip_lflows], [0], [dnl
+])
+AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep "ip6.dst == fd01::2" | ovn_strip_lflows], [0], [dnl
+])
+AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep "eth.dst == 00:00:00:00:00:01" | ovn_strip_lflows], [0], [dnl
+])
+AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep "ip6.dst == fd01::1" | ovn_strip_lflows], [0], [dnl
+])
+
+AT_CLEANUP
+])

tests/ovn.at
@@ -44371,3 +44371,80 @@ check ovn-nbctl --wait=hv sync
 OVN_CLEANUP([hv1],[hv2],[hv3])
 AT_CLEANUP
 ])
+
+OVN_FOR_EACH_NORTHD([
+AT_SETUP([Unicast ARP when proxy ARP is configured])
+AT_SKIP_IF([test $HAVE_SCAPY = no])
+ovn_start
+
+check ovn-nbctl ls-add ls1
+check ovn-nbctl lsp-add ls1 vm1 -- \
+      lsp-set-addresses vm1 "00:00:00:00:00:02 10.0.0.2 fd01::2"
+check ovn-nbctl lsp-add ls1 vm2 -- \
+      lsp-set-addresses vm2 "00:00:00:00:00:03 10.0.0.3 fd01::3"
+
+check ovn-nbctl lr-add lr1
+check ovn-nbctl lrp-add lr1 lr1-ls1 00:00:00:00:00:01 10.0.0.1 fd01::1
+check ovn-nbctl lsp-add ls1 ls1-lr1 -- \
+      lsp-set-addresses ls1-lr1 router -- \
+      lsp-set-type ls1-lr1 router -- \
+      lsp-set-options ls1-lr1 router-port=lr1-ls1
+
+check ovn-nbctl --wait=hv sync
+
+net_add n1
+sim_add hv1
+as hv1
+ovs-vsctl add-br br-phys
+ovn_attach n1 br-phys 192.168.0.1
+ovs-vsctl add-port br-int vif1 -- set Interface vif1 external-ids:iface-id=vm1 \
+    options:tx_pcap=hv1/vif1-tx.pcap \
+    options:rxq_pcap=hv1/vif1-rx.pcap
+ovs-vsctl add-port br-int vif2 -- set Interface vif2 external-ids:iface-id=vm2 \
+    options:tx_pcap=hv1/vif2-tx.pcap \
+    options:rxq_pcap=hv1/vif2-rx.pcap
+
+OVN_POPULATE_ARP
+
+wait_for_ports_up
+
+arp=$(fmt_pkt "Ether(dst='00:00:00:00:00:02', src='00:00:00:00:00:03')/ \
+               ARP(hwsrc='00:00:00:00:00:03', hwdst='00:00:00:00:00:02',
+                   psrc='10.0.0.3', pdst='10.0.0.2')")
+nd_ns=$(fmt_pkt "Ether(dst='00:00:00:00:00:02', src='00:00:00:00:00:03')/ \
+                 IPv6(src='fd01::3', dst='fd01::2')/ \
+                 ICMPv6ND_NS(tgt='fd01::2')")
+
+# If we send a unicast ARP from vm2 towards vm1, the ARP should be forwarded
+# to vm1 by ls1.
+as hv1 ovs-appctl netdev-dummy/receive vif2 $arp
+echo $arp > expected
+OVN_CHECK_PACKETS([hv1/vif1-tx.pcap], expected)
+
+# And if we send a unicast ND_NS from vm2 towards vm1, the ND_NS should be
+# forwarded to vm1 by ls1.
+as hv1 ovs-appctl netdev-dummy/receive vif2 $nd_ns
+echo $nd_ns >> expected
+OVN_CHECK_PACKETS([hv1/vif1-tx.pcap], expected)
+
+
+# Add ARP proxy configuration on the router port. The subnet for the ARP proxy
+# addresses overlaps with the VIF addresses for vm1 and vm2.
+check ovn-nbctl set logical_switch_port ls1-lr1 options:arp_proxy="10.0.0.0/8 fd01::/64"
+check ovn-nbctl --wait=hv sync
+
+# Sending a unicast ARP from vm2 towards vm1 should still result in the ARP being
+# forwarded to vm1 by ls1.
+as hv1 ovs-appctl netdev-dummy/receive vif2 $arp
+echo $arp >> expected
+OVN_CHECK_PACKETS([hv1/vif1-tx.pcap], expected)
+
+# Sending a unicast ND_NS from vm2 towards vm1 should still result in the ND_NS being
+# forwarded to vm1 by ls1.
+as hv1 ovs-appctl netdev-dummy/receive vif2 $nd_ns
+echo $nd_ns >> expected
+OVN_CHECK_PACKETS([hv1/vif1-tx.pcap], expected)
+
+OVN_CLEANUP([hv1])
+AT_CLEANUP
+])

commit 52afe6244b39702779bd49e81c81db03cb6ce259

northd: Skip unicast ARP bypass for router ports.
Commit c73f7912665d ("northd: Ensure unicast ARPs are forwarded
properly.") added priority-50 "next;" flows in ls_in_arp_rsp to
bypass the proxy ARP responder for unicast ARPs targeting known
port IPs.  The bypass was intended to prevent the proxy ARP
responder (priority-30) from incorrectly answering unicast ARPs
for VIF IPs that happen to fall within the proxy ARP CIDR range.

However, the bypass was applied unconditionally to all port types,
including router ports.  When a VIF sends a unicast ARP for the
router IP (which is within the proxy ARP CIDR range), the bypass
flow intercepts it and forwards it through the pipeline instead
of letting the proxy ARP responder answer.  The ARP then reaches
ls_in_l2_lkup where it is flooded.  On switches with 200+ ports,
this flooding can exceed the OVS 4096 resubmit limit and drop
the packet entirely.

Fix this by excluding router-type LSP ports from the unicast ARP
and ND bypass.  Router port IPs within the proxy ARP range should
be answered by the proxy ARP responder, which is their intended
purpose.  VIF ports continue to get the bypass, preserving the
fix for the original proxy ARP false-positive issue.

Fixes: c73f7912665d ("northd: Ensure unicast ARPs are forwarded properly.")
Reported-at: https://issues.redhat.com/browse/FDP-3885
Assisted-by: Claude Opus 4.6, Claude Code
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

northd/northd.c
@@ -10501,8 +10501,12 @@ build_lswitch_arp_nd_responder_known_ips(struct ovn_port *op,
                  * Without proxy arp configured, these flows are
                  * unnecessary since the packets will hit the default
                  * "next" flow at priority 0.
+                 *
+                 * Router ports are excluded: their IPs should be answered
+                 * by the proxy ARP responder (priority 30), not bypassed.
                  */
-                if (op->od->has_arp_proxy_port) {
+                if (op->od->has_arp_proxy_port
+                    && !lsp_is_router(op->nbsp)) {
                     size_t match_len = match->length;
                     ds_put_format(match, " && eth.dst == %s",
                                   op->lsp_addrs[i].ea_s);
@@ -10563,9 +10567,13 @@ build_lswitch_arp_nd_responder_known_ips(struct ovn_port *op,
              *     with IPv4, if proxy ARP is configured, then we need to
              *     install unicast nd_ns flows that allow the packet to reach
              *     its target as intended.
+             *
+             *   - Router ports are excluded: their IPs should be answered
+             *     by the proxy ARP/ND responder (priority 30), not bypassed.
              */
             for (size_t j = 0; j < op->lsp_addrs[i].n_ipv6_addrs; j++) {
-                if (op->od->has_arp_proxy_port) {
+                if (op->od->has_arp_proxy_port
+                    && !lsp_is_router(op->nbsp)) {
                     ds_clear(match);
                     ds_put_format(match,
                                   "nd_ns && ip6.dst == %s && nd.target == %s",

tests/ovn-northd.at
@@ -20037,13 +20037,13 @@ AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep "ip6.dst == fd01:
   table=??(ls_in_arp_rsp      ), priority=50   , match=(nd_ns && ip6.dst == fd01::2 && nd.target == fd01::2), action=(next;)
 ])
 
-# We should also see an ARP flow for ls1-lr1's MAC.
+# We should NOT see an ARP flow for ls1-lr1's MAC. Router ports should not
+# get unicast ARP bypass flows because proxy ARP (P30) should answer for
+# router IPs within the proxy ARP range.
 AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep "eth.dst == 00:00:00:00:00:01" | ovn_strip_lflows], [0], [dnl
-  table=??(ls_in_arp_rsp      ), priority=50   , match=(arp.tpa == 192.168.0.1 && arp.op == 1 && eth.dst == 00:00:00:00:00:01), action=(next;)
 ])
-# And we should see an ND flow for ls1-lr1's IPv6 address.
+# And we should NOT see a unicast ND flow for ls1-lr1's IPv6 address either.
 AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep "ip6.dst == fd01::1" | ovn_strip_lflows], [0], [dnl
-  table=??(ls_in_arp_rsp      ), priority=50   , match=(nd_ns && ip6.dst == fd01::1 && nd.target == fd01::1), action=(next;)
 ])
 
 check ovn-nbctl remove logical_switch_port ls1-lr1 options arp_proxy
@@ -20058,6 +20058,40 @@ AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep "eth.dst == 00:00
 ])
 AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep "ip6.dst == fd01::1" | ovn_strip_lflows], [0], [dnl
 ])
+
+OVN_CLEANUP_NORTHD
+AT_CLEANUP
+])
+
+OVN_FOR_EACH_NORTHD_NO_HV([
+AT_SETUP([Unicast ARP bypass should not apply to router ports])
+ovn_start
+
+check ovn-nbctl ls-add ls1
+check ovn-nbctl lsp-add ls1 vm1 -- \
+      lsp-set-addresses vm1 "00:00:00:00:00:02 10.0.0.2"
+
+check ovn-nbctl lr-add lr1
+check ovn-nbctl lrp-add lr1 lr1-ls1 00:00:00:00:00:01 10.0.0.1/24
+check ovn-nbctl lsp-add-router-port ls1 ls1-lr1 lr1-ls1
+
+dnl Configure proxy ARP on the router port with a range that includes the
+dnl router IP (10.0.0.1 is within 10.0.0.0/24).
+check ovn-nbctl set logical_switch_port ls1-lr1 options:arp_proxy="10.0.0.0/24"
+check ovn-nbctl --wait=sb sync
+
+dnl The P50 unicast ARP bypass should be created for the VIF port (vm1) so
+dnl that unicast ARPs for 10.0.0.2 are forwarded to vm1 rather than being
+dnl intercepted by the proxy ARP responder.
+AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep -q "eth.dst == 00:00:00:00:00:02"])
+
+dnl The P50 unicast ARP bypass should NOT be created for the router port
+dnl (ls1-lr1).  Unicast ARPs for the router IP (10.0.0.1) should be answered
+dnl by the P30 proxy ARP responder, not bypassed.  Creating the bypass
+dnl prevents the proxy ARP responder from answering, causing the ARP to be
+dnl flooded in ls_in_l2_lkup which can exceed the 4096 resubmit limit on
+dnl switches with many ports.
+AT_CHECK([ovn-sbctl lflow-list ls1 | grep ls_in_arp_rsp | grep -q "eth.dst == 00:00:00:00:00:01"], [1])
 
 OVN_CLEANUP_NORTHD
 AT_CLEANUP

tests/ovn.at
@@ -44910,6 +44910,101 @@ AT_CLEANUP
 ])
 
 OVN_FOR_EACH_NORTHD([
+AT_SETUP([Unicast ARP for router IP with proxy ARP - resubmit overflow])
+AT_KEYWORDS([slowtest])
+ovn_start
+
+dnl Create a logical switch and router, connected via a router port.
+check ovn-nbctl ls-add ls1
+check ovn-nbctl lr-add lr1
+check ovn-nbctl lrp-add lr1 lr1-ls1 00:00:00:00:00:01 10.0.0.1/24
+check ovn-nbctl lsp-add-router-port ls1 ls1-lr1 lr1-ls1
+
+dnl Configure proxy ARP on the router port with a range that includes the
+dnl router IP (10.0.0.1 is within 10.0.0.0/24).
+check ovn-nbctl set logical_switch_port ls1-lr1 \
+    options:arp_proxy="10.0.0.0/24"
+
+dnl Add a VIF port that will send the unicast ARP.
+check ovn-nbctl lsp-add ls1 vm1 -- \
+      lsp-set-addresses vm1 "00:00:00:00:00:02 10.0.0.2"
+
+dnl Add 220 additional VIF ports to make MC_FLOOD_L2 large enough
+dnl that flooding to it would exceed the OVS 4096 resubmit limit.
+dnl On release branches where ls_in_l2_lkup P80 uses MC_FLOOD_L2
+dnl instead of MC_UNKNOWN, this causes the packet to be dropped.
+nbctl_cmd=""
+for i in $(seq 1 220); do
+    idx=$(printf "%04d" $i)
+    mac=$(printf "00:00:01:%02x:%02x:%02x" $(($i / 65536 % 256)) $(($i / 256 % 256)) $(($i % 256)))
+    ip=$(printf "10.0.%d.%d" $(( ($i + 10) / 256)) $(( ($i + 10) % 256)))
+    nbctl_cmd="$nbctl_cmd -- lsp-add ls1 vif-$idx -- lsp-set-addresses vif-$idx \"$mac $ip\""
+done
+eval check ovn-nbctl $nbctl_cmd
+
+net_add n1
+sim_add hv1
+as hv1
+ovs-vsctl add-br br-phys
+ovn_attach n1 br-phys 192.168.0.1
+
+dnl Bind the sender VIF port.
+ovs-vsctl add-port br-int vif-vm1 -- \
+    set Interface vif-vm1 external-ids:iface-id=vm1 \
+    options:tx_pcap=hv1/vif-vm1-tx.pcap \
+    options:rxq_pcap=hv1/vif-vm1-rx.pcap
+
+dnl Bind all 220 additional VIF ports so they join MC_FLOOD_L2.
+vsctl_cmd=""
+for i in $(seq 1 220); do
+    idx=$(printf "%04d" $i)
+    vsctl_cmd="$vsctl_cmd -- add-port br-int vif-$idx -- set Interface vif-$idx external-ids:iface-id=vif-$idx"
+done
+eval check ovs-vsctl $vsctl_cmd
+
+OVN_POPULATE_ARP
+wait_for_ports_up vm1
+
+check ovn-nbctl --wait=hv sync
+
+dnl Trace a unicast ARP request from vm1 for the router IP.
+dnl This is a normal ARP request (NOT a GARP): arp.spa != arp.tpa.
+dnl eth.dst is the router MAC (unicast).
+dnl
+dnl Correct behavior: the P30 proxy ARP responder in ls_in_arp_rsp
+dnl should answer with an ARP reply, since 10.0.0.1 is within the
+dnl proxy ARP range 10.0.0.0/24.  The reply is generated directly
+dnl in the switch pipeline without any cloning or flooding.
+dnl
+dnl Buggy behavior: the P50 bypass flow created by commit c73f7912665d
+dnl for the router port IP prevents the proxy ARP responder from
+dnl answering.  The ARP passes through to ls_in_l2_lkup where it is
+dnl flooded.  On release branches (without MC_UNKNOWN fix), this
+dnl floods to MC_FLOOD_L2 with 220+ members, exceeding the 4096
+dnl resubmit limit and dropping the packet.
+
+dnl Verify via ofproto/trace that the packet is not dropped.
+packet="in_port=vif-vm1,dl_src=00:00:00:00:00:02,dl_dst=00:00:00:00:00:01,arp,arp_spa=10.0.0.2,arp_tpa=10.0.0.1,arp_op=1,arp_sha=00:00:00:00:00:02,arp_tha=00:00:00:00:00:01"
+AT_CHECK([as hv1 ovs-appctl ofproto/trace br-int "$packet" 2>&1 | tee trace-output], [0], [stdout])
+AT_CHECK([grep -q "packet is dropped" trace-output], [1])
+
+dnl Verify via ovn-trace that the proxy ARP responder handles
+dnl the packet directly (no clone/flood via ls_in_l2_lkup).
+dnl When the proxy ARP responder answers, the output is a direct
+dnl ARP reply without a "clone" wrapper.  When the bypass causes
+dnl the packet to pass through to ls_in_l2_lkup, ovn-trace shows
+dnl a "clone" action.
+AT_CHECK([ovn_trace --minimal ls1 \
+    'inport == "vm1" && eth.src == 00:00:00:00:00:02 && eth.dst == 00:00:00:00:00:01 && arp.spa == 10.0.0.2 && arp.tpa == 10.0.0.1 && arp.op == 1' \
+    2>/dev/null], [0], [stdout])
+AT_CHECK([grep -q "arp.op = 2" stdout])
+AT_CHECK([grep -q "clone" stdout], [1])
+
+OVN_CLEANUP([hv1])
+AT_CLEANUP
+])
+
+OVN_FOR_EACH_NORTHD([
 AT_SETUP([Port security - VRRPv3 ARP/ND])
 AT_SKIP_IF([test $HAVE_SCAPY = no])
 ovn_start