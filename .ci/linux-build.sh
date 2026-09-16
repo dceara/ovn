e54b311abb6b49b7fdc70c01b1da2291f1d9b844
Dumitru Ceara <dceara@redhat.com>
2026-09-16 12:55:07 +0200

    TODO: ci: Don't ignore testsuite stderr contents.
    For example when running with more parallel jobs make check may detect
    testsuite compilation errors and report them to stderr but will still
    return a 0 exit code:
    
      # make check TESTSUITEFLAGS="336-338 -j2"
      ...
      ## ------------------------ ##
      ## ovn 26.09.90 test suite. ##
      ## ------------------------ ##
    
      at-groups/337/test-source: line 12: syntax error near unexpected token `|'
      at-groups/337/test-source: line 12: `    test "x$(echo foobar | | grep "Status")" = "xStatus: paused"'
      336: IGMP relay - distributed gateway port -- parallelization=yes -- ovn_monitor_all=no ok
      338: MLD snoop/querier/relay -- parallelization=yes -- ovn_monitor_all=yes ok
    
      ## ------------- ##
      ## Test results. ##
      ## ------------- ##
    
      All 2 tests were successful.
    
    Spotted during an unrelated review:
    https://mail.openvswitch.org/pipermail/ovs-dev/2026-September/435956.html
    
    Signed-off-by: Dumitru Ceara <dceara@redhat.com>

--- .ci/linux-build.sh
+++ .ci/linux-build.sh
@@ -144,14 +144,25 @@ function execute_dist_tests()
 
 function run_tests()
 {
+    local errf="tests/testsuite.stderr"
+
     if ! timeout -k 5m -v $TIMEOUT make check \
         CFLAGS="${COMMON_CFLAGS} ${OVN_CFLAGS}" $JOBS \
         TESTSUITEFLAGS="$JOBS $TEST_RANGE" RECHECK=$RECHECK \
-        SKIP_UNSTABLE=$SKIP_UNSTABLE
+        SKIP_UNSTABLE=$SKIP_UNSTABLE 2> $errf
     then
         # testsuite.log is necessary for debugging.
         cat tests/testsuite.log
         return 1
+    fi
+
+    if grep -q 'tests/testsuite.dir' $errf; then
+        # Exit early in case testsuite compilation errors have been
+        # detected.  We don't return and exit directly in order to make
+        # this error as visible as possible.
+        echo "Potential test compilation errors in stderr output."
+        cat $errf
+        exit 1
     fi
 }
 
